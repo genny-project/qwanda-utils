c36fdfb3d2045ea8757d49c2de72d8233c1c08be
andres <helios@andress-MBP.lan>
2017-10-10 14:31:15 +1100

    Added QwandaTest test case for retrieving BaseEntities

--- src/test/java/life/genny/QwandaTest.java
+++ src/test/java/life/genny/QwandaTest.java
@@ -1,70 +1,28 @@
 package life.genny;
 
-//import java.io.IOException;
-//import java.time.LocalDateTime;
-//
-//import org.junit.Test;
-//
-//import com.google.gson.Gson;
-//import com.google.gson.GsonBuilder;
-//
-//import life.genny.qwanda.Answer;
-//import life.genny.qwanda.DateTimeDeserializer;
-//import life.genny.qwandautils.KeycloakUtils;
-//import life.genny.qwandautils.QwandaUtils;
+import static java.lang.System.out;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import org.junit.Test;
+import life.genny.qwanda.entity.BaseEntity;
+import life.genny.qwandautils.GennySheets;
 
 public class QwandaTest {
 
-//	@Test 
-//	// run the keycloak docker located in the root of the project runKeycloak.sh
-//	public void tokenTest() {
-//		try {
-//		String tokenString = KeycloakUtils.getToken("http://localhost:8180",
-//					"wildfly-swarm-keycloak-example", "curl", "056b73c1-7078-411d-80ec-87d41c55c3b4", "user1", "password1");
-//			System.out.println(tokenString);
-//			
-//			// fetch qwanda details
-//			String json = QwandaUtils.apiGet("http://localhost:8180/qwanda/asks", tokenString);
-//			System.out.println("asks:"+json);
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//
-//	}
-//	
-//	@Test
-//	public void JsonArrayTest()
-//	{
-//		try {
-//		String tokenString = KeycloakUtils.getToken("http://localhost:8180",
-//				"wildfly-swarm-keycloak-example", "curl", "056b73c1-7078-411d-80ec-87d41c55c3b4", "user1", "password1");
-//			String json = QwandaUtils.apiGet("http://localhost:8180/qwanda/asks", tokenString);
-////			Gson gson = new GsonBuilder().registerTypeAdapter(LocalDateTime.class, new JsonDeserializer<LocalDateTime>() {
-////				@Override
-////				public LocalDateTime deserialize(JsonElement json, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
-////				    return ZonedDateTime.parse(json.getAsJsonPrimitive().getAsString()).toLocalDateTime();
-////				}
-////				}).create();			
-////			System.out.println(json);
-//			GsonBuilder gsonBuilder = new GsonBuilder();
-//			gsonBuilder.registerTypeAdapter(LocalDateTime.class, new DateTimeDeserializer());
-//
-//			Gson gson = gsonBuilder.create();			
-//			Answer ans = new Answer("PER_BOB", "PER_BOB", "PRI_FIRSTNAME","Bob");
-//			String answerString = gson.toJson(ans);
-//	
-////			String answerStr = "{\"created\":\"2017-03-02T10:11:12.000+10:00\",\"value\":\"Bob2\",\"attributeCode\":\"PRI_FIRSTNAME\",\"targetCode\":22,\"sourceCode\":20,\"expired\":false,\"refused\":false,\"weight\":0.5}";
-////			Answer  answer = gson.fromJson(answerStr, Answer.class);
-//			System.out.println(answerString);
-////			QDataAskMessage askMsg = new QDataAskMessage(asks);
-////			
-////	
-////			System.out.println(askMsg);
-//////			System.out.println(jsonArray.get(0));
-//	} catch (IOException e) {
-//		// TODO Auto-generated catch block
-//		e.printStackTrace();
-//	}
-//	}
+  @Test
+  public void getData() {
+    GennySheets sheets = new GennySheets(
+        "{\"installed\":{\"client_id\":\"260075856207-9d7a02ekmujr2bh7i53dro28n132iqhe.apps.googleusercontent.com\",\"project_id\":\"genny-sheets-181905\",\"auth_uri\":\"https://accounts.google.com/o/oauth2/auth\",\"token_uri\":\"https://accounts.google.com/o/oauth2/token\",\"auth_provider_x509_cert_url\":\"https://www.googleapis.com/oauth2/v1/certs\",\"client_secret\":\"vgXEFRgQvh3_t_e5Hj-eb6IX\",\"redirect_uris\":[\"urn:ietf:wg:oauth:2.0:oob\",\"http://localhost\"]}}",
+        "1VSXJUn8_BHG1aW0DQrFDnvLjx_jxcNiD33QzqO5D-jc", new File(System.getProperty("user.home"),
+            ".credentials/sheets.googleapis.com-java-quickstart"));
+    List<BaseEntity> bes = null;
+    try {
+      bes = sheets.getBeans(BaseEntity.class);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    bes.forEach(out::println);
+  }
 }
