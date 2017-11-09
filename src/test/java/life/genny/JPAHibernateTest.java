a2f5cadc4d1d7b62c25ddcc4bf31fae368ea7ae1
andres <helios@andress-MBP.lan>
2017-11-09 20:58:05 +1100

    version of qwanda updated in pom file

--- src/test/java/life/genny/JPAHibernateTest.java
+++ src/test/java/life/genny/JPAHibernateTest.java
@@ -54,7 +54,7 @@ public class JPAHibernateTest {
   public static void init() throws FileNotFoundException, SQLException {
     emf = Persistence.createEntityManagerFactory("mnf-pu-test");
     em = emf.createEntityManager();
-    import_from_google_docs();
+//    import_from_google_docs();
   }
 
   // @Before
@@ -74,190 +74,190 @@ public class JPAHibernateTest {
   }
 
   // @Before
-  public static void import_from_google_docs() {
-
-    service = new BaseEntityService(em);
-    em.getTransaction().begin();
-
-    System.out
-        .println("**************************** IMPORTING  *************************************");
-
-    final GennySheets gennySheets = new GennySheets(secret, gennySheetId, credentialPath);
-
-
-    System.out.println(
-        "**************************** IMPORTING VALIDATIONS *************************************");
-
-    // Validations
-    final Map<String, Validation> validationMap = new HashMap<String, Validation>();
-
-    List<Validation> validations = null;
-    try {
-      validations = gennySheets.getBeans(Validation.class);
-    } catch (final IOException e2) {
-      // TODO Auto-generated catch block
-      e2.printStackTrace();
-    }
-    validations.stream().forEach(object -> {
-      service.insert(object);
-      validationMap.put(object.getCode(), object);
-    });
-
-    System.out.println(
-        "**************************** IMPORTING DATATYPES *************************************");
-
-    // DataTypes
-    final Map<String, DataType> dataTypeMap = new HashMap<String, DataType>();
-    List<Map> obj = null;
-    try {
-      obj = gennySheets.row2DoubleTuples(DataType.class.getSimpleName());
-    } catch (final IOException e1) {
-      // TODO Auto-generated catch block
-      e1.printStackTrace();
-    }
-    obj.stream().forEach(object -> {
-      final String code = (String) object.get("code");
-      final String name = (String) object.get("name");
-      final String validationss = (String) object.get("validations");
-      final ValidationList validationList = new ValidationList();
-      validationList.setValidationList(new ArrayList<Validation>());
-      if (validationss != null) {
-        final String[] validationListStr = validationss.split(",");
-        for (final String validationCode : validationListStr) {
-          validationList.getValidationList().add(validationMap.get(validationCode));
-        }
-      }
-      if (!dataTypeMap.containsKey(code)) {
-        final DataType dataType = new DataType(name, validationList);
-        dataTypeMap.put(code, dataType);
-      }
-    });
-
-    System.out.println(
-        "**************************** IMPORTING ATTRIBUTES *************************************");
-
-    List<Map> attrs = null;
-    try {
-      attrs = gennySheets.row2DoubleTuples(Attribute.class.getSimpleName());
-    } catch (final IOException e2) {
-      // TODO Auto-generated catch block
-      e2.printStackTrace();
-    }
-    attrs.stream().forEach(data -> {
-      Attribute attribute = null;
-      final String datatype = (String) data.get("datatype");
-      try {
-        System.out.println("ATTRIBUTE:::Code:" + data.get("code") + ":name" + data.get("name")
-            + ":datatype:" + data.get("datatype"));
-        attribute = service.findAttributeByCode((String) data.get("code"));
-
-      } catch (final NoResultException e) {
-        attribute = new Attribute((String) data.get("code"), (String) data.get("name"),
-            dataTypeMap.get(datatype));
-        // System.out.println("ATTRIBUTE:::Code:" + data.get("code") + ":name" + data.get("name")
-        // + ":datatype:" + data.get("datatype") + "##################" + attribute);
-        service.insert(attribute);
-      } catch (final OptimisticLockException ee) {
-        attribute = new Attribute((String) data.get("code"), (String) data.get("name"),
-            dataTypeMap.get(datatype));
-        // System.out.println("ATTRIBUTE:::Code:" + data.get("code") + ":name" + data.get("name")
-        // + ":datatype:" + data.get("datatype") + "##################" + attribute);
-        // service.insert(attribute);
-      }
-    });
-    System.out.println(
-        "**************************** IMPORTING BASEENTITYS *************************************");
-
-    List<BaseEntity> bes = null;
-    try {
-      bes = gennySheets.getBeans(BaseEntity.class);
-      bes.stream().forEach(object -> {
-        service.insert(object);
-      });
-    } catch (final IOException e2) {
-      // TODO Auto-generated catch block
-      e2.printStackTrace();
-    }
-
-
-    if (true) {
-      System.out.println(
-          "**************************** IMPORTING BASEENTITY - ATTRIBUTE LINKS *************************************");
-
-      // Now link Attributes to Baseentitys
-      List<Map> obj2 = null;
-      try {
-        obj2 = gennySheets.row2DoubleTuples(EntityAttribute.class.getSimpleName());
-      } catch (final IOException e1) {
-        // TODO Auto-generated catch block
-        e1.printStackTrace();
-      }
-      System.out.println("Number of Links = " + obj2.size());
-
-      obj2.stream().forEach(object -> {
-        final String beCode = (String) object.get("baseEntityCode");
-        final String attributeCode = (String) object.get("attributeCode");
-        final String weightStr = (String) object.get("weight");
-        final String valueString = (String) object.get("valueString");
-        System.out.println("BECode:" + beCode + ":attCode" + attributeCode + ":weight:" + weightStr
-            + ": valueString:" + valueString);
-        Attribute attribute = null;
-        BaseEntity be = null;
-        try {
-          attribute = service.findAttributeByCode(attributeCode);
-          be = service.findBaseEntityByCode(beCode);
-          System.out.println("In EA : found " + be.getCode() + "," + attribute.getCode());
-          final Double weight = Double.valueOf(weightStr);
-          try {
-            be.addAttribute(attribute, weight, valueString);
-
-          } catch (final BadDataException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-          }
-
-          service.update(be);
-        } catch (final NoResultException e) {
-
-        }
-      });
-
-      // Now link be to be
-      final AttributeLink linkAttribute2 = new AttributeLink("LNK_CORE", "Parent");
-      service.insert(linkAttribute2);
-
-      List<Map> obj3 = null;
-      try {
-        obj3 = gennySheets.row2DoubleTuples(EntityEntity.class.getSimpleName());
-      } catch (final IOException e1) {
-        // TODO Auto-generated catch block
-        e1.printStackTrace();
-      }
-      obj3.stream().forEach(object -> {
-        final String parentCode = (String) object.get("parentCode");
-        final String targetCode = (String) object.get("targetCode");
-        object.get("linkCode");
-        final String weightStr = (String) object.get("weight");
-        object.get("valueString");
-        BaseEntity sbe = null;
-        BaseEntity tbe = null;
-        try {
-          sbe = service.findBaseEntityByCode(parentCode);
-          tbe = service.findBaseEntityByCode(targetCode);
-          final Double weight = Double.valueOf(weightStr);
-          sbe.addTarget(tbe, linkAttribute2, weight);
-          service.update(tbe);
-        } catch (final NoResultException e) {
-
-        } catch (final BadDataException e) {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
-        }
-      });
-    }
-
-    em.getTransaction().commit();
-  }
+//  public static void import_from_google_docs() {
+//
+//    service = new BaseEntityService(em);
+//    em.getTransaction().begin();
+//
+//    System.out
+//        .println("**************************** IMPORTING  *************************************");
+//
+//    final GennySheets gennySheets = new GennySheets(secret, gennySheetId, credentialPath);
+//
+//
+//    System.out.println(
+//        "**************************** IMPORTING VALIDATIONS *************************************");
+//
+//    // Validations
+//    final Map<String, Validation> validationMap = new HashMap<String, Validation>();
+//
+//    List<Validation> validations = null;
+//    try {
+//      validations = gennySheets.getBeans(Validation.class);
+//    } catch (final IOException e2) {
+//      // TODO Auto-generated catch block
+//      e2.printStackTrace();
+//    }
+//    validations.stream().forEach(object -> {
+//      service.insert(object);
+//      validationMap.put(object.getCode(), object);
+//    });
+//
+//    System.out.println(
+//        "**************************** IMPORTING DATATYPES *************************************");
+//
+//    // DataTypes
+//    final Map<String, DataType> dataTypeMap = new HashMap<String, DataType>();
+//    List<Map> obj = null;
+//    try {
+//      obj = gennySheets.row2DoubleTuples(DataType.class.getSimpleName());
+//    } catch (final IOException e1) {
+//      // TODO Auto-generated catch block
+//      e1.printStackTrace();
+//    }
+//    obj.stream().forEach(object -> {
+//      final String code = (String) object.get("code");
+//      final String name = (String) object.get("name");
+//      final String validationss = (String) object.get("validations");
+//      final ValidationList validationList = new ValidationList();
+//      validationList.setValidationList(new ArrayList<Validation>());
+//      if (validationss != null) {
+//        final String[] validationListStr = validationss.split(",");
+//        for (final String validationCode : validationListStr) {
+//          validationList.getValidationList().add(validationMap.get(validationCode));
+//        }
+//      }
+//      if (!dataTypeMap.containsKey(code)) {
+//        final DataType dataType = new DataType(name, validationList);
+//        dataTypeMap.put(code, dataType);
+//      }
+//    });
+//
+//    System.out.println(
+//        "**************************** IMPORTING ATTRIBUTES *************************************");
+//
+//    List<Map> attrs = null;
+//    try {
+//      attrs = gennySheets.row2DoubleTuples(Attribute.class.getSimpleName());
+//    } catch (final IOException e2) {
+//      // TODO Auto-generated catch block
+//      e2.printStackTrace();
+//    }
+//    attrs.stream().forEach(data -> {
+//      Attribute attribute = null;
+//      final String datatype = (String) data.get("datatype");
+//      try {
+//        System.out.println("ATTRIBUTE:::Code:" + data.get("code") + ":name" + data.get("name")
+//            + ":datatype:" + data.get("datatype"));
+//        attribute = service.findAttributeByCode((String) data.get("code"));
+//
+//      } catch (final NoResultException e) {
+//        attribute = new Attribute((String) data.get("code"), (String) data.get("name"),
+//            dataTypeMap.get(datatype));
+//        // System.out.println("ATTRIBUTE:::Code:" + data.get("code") + ":name" + data.get("name")
+//        // + ":datatype:" + data.get("datatype") + "##################" + attribute);
+//        service.insert(attribute);
+//      } catch (final OptimisticLockException ee) {
+//        attribute = new Attribute((String) data.get("code"), (String) data.get("name"),
+//            dataTypeMap.get(datatype));
+//        // System.out.println("ATTRIBUTE:::Code:" + data.get("code") + ":name" + data.get("name")
+//        // + ":datatype:" + data.get("datatype") + "##################" + attribute);
+//        // service.insert(attribute);
+//      }
+//    });
+//    System.out.println(
+//        "**************************** IMPORTING BASEENTITYS *************************************");
+//
+//    List<BaseEntity> bes = null;
+//    try {
+//      bes = gennySheets.getBeans(BaseEntity.class);
+//      bes.stream().forEach(object -> {
+//        service.insert(object);
+//      });
+//    } catch (final IOException e2) {
+//      // TODO Auto-generated catch block
+//      e2.printStackTrace();
+//    }
+//
+//
+//    if (true) {
+//      System.out.println(
+//          "**************************** IMPORTING BASEENTITY - ATTRIBUTE LINKS *************************************");
+//
+//      // Now link Attributes to Baseentitys
+//      List<Map> obj2 = null;
+//      try {
+//        obj2 = gennySheets.row2DoubleTuples(EntityAttribute.class.getSimpleName());
+//      } catch (final IOException e1) {
+//        // TODO Auto-generated catch block
+//        e1.printStackTrace();
+//      }
+//      System.out.println("Number of Links = " + obj2.size());
+//
+//      obj2.stream().forEach(object -> {
+//        final String beCode = (String) object.get("baseEntityCode");
+//        final String attributeCode = (String) object.get("attributeCode");
+//        final String weightStr = (String) object.get("weight");
+//        final String valueString = (String) object.get("valueString");
+//        System.out.println("BECode:" + beCode + ":attCode" + attributeCode + ":weight:" + weightStr
+//            + ": valueString:" + valueString);
+//        Attribute attribute = null;
+//        BaseEntity be = null;
+//        try {
+//          attribute = service.findAttributeByCode(attributeCode);
+//          be = service.findBaseEntityByCode(beCode);
+//          System.out.println("In EA : found " + be.getCode() + "," + attribute.getCode());
+//          final Double weight = Double.valueOf(weightStr);
+//          try {
+//            be.addAttribute(attribute, weight, valueString);
+//
+//          } catch (final BadDataException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//          }
+//
+//          service.update(be);
+//        } catch (final NoResultException e) {
+//
+//        }
+//      });
+//
+//      // Now link be to be
+//      final AttributeLink linkAttribute2 = new AttributeLink("LNK_CORE", "Parent");
+//      service.insert(linkAttribute2);
+//
+//      List<Map> obj3 = null;
+//      try {
+//        obj3 = gennySheets.row2DoubleTuples(EntityEntity.class.getSimpleName());
+//      } catch (final IOException e1) {
+//        // TODO Auto-generated catch block
+//        e1.printStackTrace();
+//      }
+//      obj3.stream().forEach(object -> {
+//        final String parentCode = (String) object.get("parentCode");
+//        final String targetCode = (String) object.get("targetCode");
+//        object.get("linkCode");
+//        final String weightStr = (String) object.get("weight");
+//        object.get("valueString");
+//        BaseEntity sbe = null;
+//        BaseEntity tbe = null;
+//        try {
+//          sbe = service.findBaseEntityByCode(parentCode);
+//          tbe = service.findBaseEntityByCode(targetCode);
+//          final Double weight = Double.valueOf(weightStr);
+//          sbe.addTarget(tbe, linkAttribute2, weight);
+//          service.update(tbe);
+//        } catch (final NoResultException e) {
+//
+//        } catch (final BadDataException e) {
+//          // TODO Auto-generated catch block
+//          e.printStackTrace();
+//        }
+//      });
+//    }
+//
+//    em.getTransaction().commit();
+//  }
 
 //  public Map<String, Object> genny(final String projectType, final File path) {
 //
