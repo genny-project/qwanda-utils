ef2174ec317e59aa01c99920fded9a263136321a
Sudan <sudansth@gmail.com>
2017-11-23 12:06:03 +1100

    Added method getLatLong() to get lat and long of the given address from google api

--- src/main/java/life/genny/qwandautils/QwandaUtils.java
+++ src/main/java/life/genny/qwandautils/QwandaUtils.java
@@ -11,10 +11,24 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-import java.io.BufferedReader;
+
+
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
+
+import java.net.URLConnection;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+import javax.xml.xpath.XPathConstants;
+import org.w3c.dom.Document;
 
 public class QwandaUtils {
 	public static String apiGet(final String getUrl, final String authToken)
@@ -79,5 +93,41 @@ public class QwandaUtils {
 		}
 		return retJson;
 	}
+	
+	
+	//Gives the latitude and longitude of the given address
+	public static String[] getLatLong(String address) throws Exception
+	{
+		int responseCode=0;
+		String api = "http://maps.googleapis.com/maps/api/geocode/xml?address=" + URLEncoder.encode(address, "UTF-8") + "&sensor=true";
+	    System.out.println("API URL : "+api);
+	    URL url = new URL(api);
+	    HttpURLConnection httpConnection = (HttpURLConnection)url.openConnection();
+	    httpConnection.connect();
+	    responseCode = httpConnection.getResponseCode();
+	    if(responseCode == 200)
+	    {
+	      DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();;
+	      Document document = dBuilder.parse(httpConnection.getInputStream());
+	      XPathFactory xPathfactory = XPathFactory.newInstance();
+	      XPath xpath = xPathfactory.newXPath();
+	      XPathExpression expr = xpath.compile("/GeocodeResponse/status");
+	      String status = (String)expr.evaluate(document, XPathConstants.STRING);
+	      if(status.equals("OK"))
+	      {
+	         expr = xpath.compile("//geometry/location/lat");
+	         String latitude = (String)expr.evaluate(document, XPathConstants.STRING);
+	         expr = xpath.compile("//geometry/location/lng");
+	         String longitude = (String)expr.evaluate(document, XPathConstants.STRING);
+	         return new String[] {latitude, longitude};
+	      }
+	      else
+	      {
+	         throw new Exception("Error from the API - response status: "+status);
+	      }
+	    }
+	    return null;
+	  }
+	
 
 }
