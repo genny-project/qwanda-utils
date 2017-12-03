15403633d920e9ca6f0914558bd9f14b75400ae5
Adam Crow <acrow@Adams-MacBook-Pro.local>
2017-12-04 00:56:02 +1100

    Added keycloakUtil token fetcher

--- src/main/java/life/genny/qwandautils/KeycloakUtils.java
+++ src/main/java/life/genny/qwandautils/KeycloakUtils.java
@@ -6,6 +6,13 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 // import org.keycloak.OAuth2Constants;
 // import org.keycloak.common.util.KeycloakUriBuilder;
 // import org.keycloak.constants.ServiceUrlConstants;
@@ -13,154 +20,168 @@ import org.apache.http.HttpEntity;
 // import org.keycloak.util.JsonSerialization;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.common.util.KeycloakUriBuilder;
+import org.keycloak.constants.ServiceUrlConstants;
+import org.keycloak.representations.AccessTokenResponse;
+import org.keycloak.util.JsonSerialization;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class KeycloakUtils {
 
-  // static public String getToken(String keycloakUrl, String realm, String clientId, String secret,
-  // String username, String password) throws IOException {
-  //
-  // return getAccessToken(keycloakUrl,realm,clientId,secret,username,password).getToken();
-  // }
+	// static public String getToken(String keycloakUrl, String realm, String
+	// clientId, String secret,
+	// String username, String password) throws IOException {
+	//
+	// return
+	// getAccessToken(keycloakUrl,realm,clientId,secret,username,password).getToken();
+	// }
 
-  // static public AccessTokenResponse getAccessToken(String keycloakUrl, String realm, String
-  // clientId, String secret, String username, String password) throws IOException {
-  //
-  // HttpClient httpClient = new DefaultHttpClient();
-  //
-  // try {
-  // HttpPost post = new HttpPost(KeycloakUriBuilder.fromUri(keycloakUrl+"/auth")
-  // .path(ServiceUrlConstants.TOKEN_PATH).build(realm));
-  // System.out.println("url token
-  // post="+keycloakUrl+"/auth"+",tokenpath="+ServiceUrlConstants.TOKEN_PATH+":realm="+realm+":clientid="+clientId+":secret"+secret+":un:"+username+"pw:"+password);;
-  // post.addHeader("Content-Type", "application/x-www-form-urlencoded");
-  //
-  // List <NameValuePair> formParams = new ArrayList <NameValuePair>();
-  // formParams.add(new BasicNameValuePair("username", username));
-  // formParams.add(new BasicNameValuePair("password", password));
-  // formParams.add(new BasicNameValuePair(OAuth2Constants.GRANT_TYPE, "password"));
-  // formParams.add(new BasicNameValuePair(OAuth2Constants.CLIENT_ID, clientId));
-  // formParams.add(new BasicNameValuePair(OAuth2Constants.CLIENT_SECRET, secret));
-  // UrlEncodedFormEntity form = new UrlEncodedFormEntity(formParams, "UTF-8");
-  //
-  // post.setEntity(form);
-  //
-  // HttpResponse response = httpClient.execute(post);
-  //
-  // int statusCode = response.getStatusLine().getStatusCode();
-  // HttpEntity entity = response.getEntity();
-  // String content = null;
-  // if (statusCode != 200) {
-  // content = getContent(entity);
-  // throw new IOException(""+statusCode);
-  // }
-  // if (entity == null) {
-  // throw new IOException("Null Entity");
-  // } else {
-  // content = getContent(entity);
-  // }
-  // return JsonSerialization.readValue(content, AccessTokenResponse.class);
-  // } finally {
-  // httpClient.getConnectionManager().shutdown();
-  // }
-  // }
+	public static AccessTokenResponse getAccessToken(String keycloakUrl, String realm, String clientId, String secret,
+			String username, String password) throws IOException {
 
-  public static String getContent(final HttpEntity httpEntity) throws IOException {
-    if (httpEntity == null)
-      return null;
-    final InputStream is = httpEntity.getContent();
-    try {
-      final ByteArrayOutputStream os = new ByteArrayOutputStream();
-      int c;
-      while ((c = is.read()) != -1) {
-        os.write(c);
-      }
-      final byte[] bytes = os.toByteArray();
-      final String data = new String(bytes);
-      return data;
-    } finally {
-      try {
-        is.close();
-      } catch (final IOException ignored) {
+		HttpClient httpClient = new DefaultHttpClient();
 
-      }
-    }
+		try {
+			HttpPost post = new HttpPost(KeycloakUriBuilder.fromUri(keycloakUrl + "/auth")
+					.path(ServiceUrlConstants.TOKEN_PATH).build(realm));
+			System.out.println("url token post=" + keycloakUrl + "/auth" + ",tokenpath="
+					+ ServiceUrlConstants.TOKEN_PATH + ":realm=" + realm + ":clientid=" + clientId + ":secret" + secret
+					+ ":un:" + username + "pw:" + password);
+			;
+			post.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
-  }
-  
-  //Decode the keycloak token string and send back in Json Format
-  public static JSONObject getDecodedToken(final String bearerToken) {
-    JSONObject jsonObj = null;
-    String decodedJson = null;
-    try {
-      final String[] jwtToken = bearerToken.split("\\.");
-      final Base64 decoder = new Base64(true);
-      final byte[] decodedClaims = decoder.decode(jwtToken[1]);
-      decodedJson = new String(decodedClaims);
-      jsonObj = new JSONObject(decodedJson);
-    } catch (final JSONException e1) {
-      System.out.println(
-          "bearerToken=" + bearerToken + "  decodedJson=" + decodedJson + ":" + e1.getMessage());
-    }
-    return jsonObj;
-  }
+			List<NameValuePair> formParams = new ArrayList<NameValuePair>();
+			formParams.add(new BasicNameValuePair("username", username));
+			formParams.add(new BasicNameValuePair("password", password));
+			formParams.add(new BasicNameValuePair(OAuth2Constants.GRANT_TYPE, "password"));
+			formParams.add(new BasicNameValuePair(OAuth2Constants.CLIENT_ID, clientId));
+			formParams.add(new BasicNameValuePair(OAuth2Constants.CLIENT_SECRET, secret));
+			UrlEncodedFormEntity form = new UrlEncodedFormEntity(formParams, "UTF-8");
 
-  //Send the decoded Json token in the map
-  public static Map<String, Object> getJsonMap(final String json) {
-    final JSONObject jsonObj = getDecodedToken(json);
-    return getJsonMap(jsonObj);
-  }
+			post.setEntity(form);
 
-  public static Map<String, Object> getJsonMap(final JSONObject jsonObj) {
-    final String json = jsonObj.toString();
-    Map<String, Object> map = new HashMap<String, Object>();
-    try {
-      final ObjectMapper mapper = new ObjectMapper();
-      // convert JSON string to Map
-      final TypeReference<HashMap<String, Object>> typeRef =
-          new TypeReference<HashMap<String, Object>>() {};
+			HttpResponse response = httpClient.execute(post);
 
-      map = mapper.readValue(json, typeRef);
+			int statusCode = response.getStatusLine().getStatusCode();
+			HttpEntity entity = response.getEntity();
+			String content = null;
+			if (statusCode != 200) {
+				content = getContent(entity);
+				throw new IOException("" + statusCode);
+			}
+			if (entity == null) {
+				throw new IOException("Null Entity");
+			} else {
+				content = getContent(entity);
+			}
+			return JsonSerialization.readValue(content, AccessTokenResponse.class);
+		} finally {
+			httpClient.getConnectionManager().shutdown();
+		}
+	}
 
-    } catch (final JsonGenerationException e) {
-      e.printStackTrace();
-    } catch (final JsonMappingException e) {
-      e.printStackTrace();
-    } catch (final IOException e) {
-      e.printStackTrace();
-    }
+	public static String getContent(final HttpEntity httpEntity) throws IOException {
+		if (httpEntity == null)
+			return null;
+		final InputStream is = httpEntity.getContent();
+		try {
+			final ByteArrayOutputStream os = new ByteArrayOutputStream();
+			int c;
+			while ((c = is.read()) != -1) {
+				os.write(c);
+			}
+			final byte[] bytes = os.toByteArray();
+			final String data = new String(bytes);
+			return data;
+		} finally {
+			try {
+				is.close();
+			} catch (final IOException ignored) {
 
-    return map;
-  }
-  
-  /*
-   * Return the Set of access roles of the user logged in based on the keycloak access_roles
-   */
-  public static HashSet<String> getRoleSet(final String role){
-	  String accessRole = role.substring(role.indexOf("[") + 1, role.indexOf("]"));
+			}
+		}
+
+	}
+
+	// Decode the keycloak token string and send back in Json Format
+	public static JSONObject getDecodedToken(final String bearerToken) {
+		JSONObject jsonObj = null;
+		String decodedJson = null;
+		try {
+			final String[] jwtToken = bearerToken.split("\\.");
+			final Base64 decoder = new Base64(true);
+			final byte[] decodedClaims = decoder.decode(jwtToken[1]);
+			decodedJson = new String(decodedClaims);
+			jsonObj = new JSONObject(decodedJson);
+		} catch (final JSONException e1) {
+			System.out.println("bearerToken=" + bearerToken + "  decodedJson=" + decodedJson + ":" + e1.getMessage());
+		}
+		return jsonObj;
+	}
+
+	// Send the decoded Json token in the map
+	public static Map<String, Object> getJsonMap(final String json) {
+		final JSONObject jsonObj = getDecodedToken(json);
+		return getJsonMap(jsonObj);
+	}
+
+	public static Map<String, Object> getJsonMap(final JSONObject jsonObj) {
+		final String json = jsonObj.toString();
+		Map<String, Object> map = new HashMap<String, Object>();
+		try {
+			final ObjectMapper mapper = new ObjectMapper();
+			// convert JSON string to Map
+			final TypeReference<HashMap<String, Object>> typeRef = new TypeReference<HashMap<String, Object>>() {
+			};
+
+			map = mapper.readValue(json, typeRef);
+
+		} catch (final JsonGenerationException e) {
+			e.printStackTrace();
+		} catch (final JsonMappingException e) {
+			e.printStackTrace();
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+
+		return map;
+	}
+
+	/*
+	 * Return the Set of access roles of the user logged in based on the keycloak
+	 * access_roles
+	 */
+	public static HashSet<String> getRoleSet(final String role) {
+		String accessRole = role.substring(role.indexOf("[") + 1, role.indexOf("]"));
 		String[] strs = accessRole.trim().split("\\s*,\\s*");
-		 HashSet<String> roles= new HashSet<String>(Arrays.asList(strs));  
-	  return roles;
-	  
-  }
-  
-  /*  For testing user with different realm name in dev env as dev/genny keycloak can't be added with other realms  
-   *  Gets the Project realm from Genny Env if it is present
-   */
-  static String projectRealm = null;
-   public static String getPRJRealmFromDevEnv() {
-	  if(System.getenv("PROJECT_REALM") != null) {
-		 projectRealm = System.getenv("PROJECT_REALM");
-	 }
-    return projectRealm;
-  }
-  
+		HashSet<String> roles = new HashSet<String>(Arrays.asList(strs));
+		return roles;
+
+	}
+
+	/*
+	 * For testing user with different realm name in dev env as dev/genny keycloak
+	 * can't be added with other realms Gets the Project realm from Genny Env if it
+	 * is present
+	 */
+	static String projectRealm = null;
+
+	public static String getPRJRealmFromDevEnv() {
+		if (System.getenv("PROJECT_REALM") != null) {
+			projectRealm = System.getenv("PROJECT_REALM");
+		}
+		return projectRealm;
+	}
 
 }
