c6dbb9f1c56d1f7da12997569e84c73554aa4cd1
rpgayatri <rpgayatri@gmail.com>
2018-01-17 15:04:12 +1100

    adding util for Payments-user profile updation

--- src/main/java/life/genny/qwandautils/PaymentUtils.java
+++ src/main/java/life/genny/qwandautils/PaymentUtils.java
@@ -6,14 +6,9 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.lang.invoke.MethodHandles;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Base64;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
-import org.apache.commons.collections.map.HashedMap;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -50,8 +45,6 @@ public class PaymentUtils {
 		authObj.put("token", paymentToken);
 		authObj.put("secret", paymentSecret);
 		
-		System.out.println("assembly auth obj::"+authObj);
-		
 		String encodedAuthString = base64Encoder(authObj.toJSONString());
 
 		return encodedAuthString;
@@ -62,7 +55,6 @@ public class PaymentUtils {
 		String encodedString = null;
 		try {
 			encodedString = Base64.getEncoder().encodeToString(plainString.getBytes("utf-8"));
-			System.out.println("encoded string ::" + encodedString);
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
@@ -77,14 +69,13 @@ public class PaymentUtils {
 		org.json.simple.JSONObject authobj = new org.json.simple.JSONObject();
 
 		decodedString = new String(Base64.getDecoder().decode(plainString));
-		System.out.println("decoded string ::" + decodedString);
+		
 		try {
 			authobj = (org.json.simple.JSONObject) parser.parse(decodedString);
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		System.out.println("decoded object ::" + authobj);
-
+	
 		return authobj;
 	}
 	
@@ -142,7 +133,6 @@ public class PaymentUtils {
 	public static String apiPutPaymentEntity(final String postUrl, final String entityString, final String authToken)
 			throws IOException {
 		String retJson = "";
-		//final HttpClient client = new DefaultHttpClient();
 		final HttpClient client = HttpClientBuilder.create().build();
 
 		final HttpPut put = new HttpPut(postUrl);
@@ -174,7 +164,7 @@ public class PaymentUtils {
 	}
 	
 	
-	public static String createAssemblyId(String token) {
+	public static String getAssemblyId(String token) {
 		
 		String userCode = getUserCode(token);
 		
@@ -213,59 +203,63 @@ public class PaymentUtils {
 		
 		if(be != null) {
 			
-			String firstName = MergeUtil.getBaseEntityAttrObjectValue(be, "PRI_FIRSTNAME").toString();
-			String lastName = MergeUtil.getBaseEntityAttrObjectValue(be, "PRI_LASTNAME").toString();
-			String dobString = MergeUtil.getBaseEntityAttrObjectValue(be, "PRI_DOB").toString();
-			String email = MergeUtil.getBaseEntityAttrObjectValue(be, "PRI_EMAIL").toString();
-			String phoneNumber = MergeUtil.getBaseEntityAttrObjectValue(be, "PRI_MOBILE").toString();
-			String addressLine1 = MergeUtil.getBaseEntityAttrObjectValue(be, "PRI_ADDRESS_ADDRESS1").toString();
-			String city = MergeUtil.getBaseEntityAttrObjectValue(be, "PRI_ADDRESS_CITY").toString();
-			String state = MergeUtil.getBaseEntityAttrObjectValue(be, "PRI_ADDRESS_STATE").toString();
-			String country = MergeUtil.getBaseEntityAttrObjectValue(be, "PRI_ADDRESS_COUNTRY").toString();
-			String postCode = MergeUtil.getBaseEntityAttrObjectValue(be, "PRI_ADDRESS_POSTCODE").toString();
+			Object firstName = MergeUtil.getBaseEntityAttrObjectValue(be, "PRI_FIRSTNAME");
+			Object lastName = MergeUtil.getBaseEntityAttrObjectValue(be, "PRI_LASTNAME");
+			Object dobString = MergeUtil.getBaseEntityAttrObjectValue(be, "PRI_DOB");
+			Object email = MergeUtil.getBaseEntityAttrObjectValue(be, "PRI_EMAIL");
+			Object phoneNumber = MergeUtil.getBaseEntityAttrObjectValue(be, "PRI_MOBILE");
+			Object addressLine1 = MergeUtil.getBaseEntityAttrObjectValue(be, "PRI_ADDRESS_ADDRESS1");
+			Object city = MergeUtil.getBaseEntityAttrObjectValue(be, "PRI_ADDRESS_CITY");
+			Object state = MergeUtil.getBaseEntityAttrObjectValue(be, "PRI_ADDRESS_STATE");
+			Object country = MergeUtil.getBaseEntityAttrObjectValue(be, "PRI_ADDRESS_COUNTRY");
+			Object postCode = MergeUtil.getBaseEntityAttrObjectValue(be, "PRI_ADDRESS_POSTCODE");
 			
 			if(firstName != null) {
-				personalInfoObj.put("firstName", firstName);
+				personalInfoObj.put("firstName", firstName.toString());
 			}
 			
 			if(lastName != null) {
-				personalInfoObj.put("lastName", lastName);
-			}
+				personalInfoObj.put("lastName", lastName.toString());
+			} 
 			
 			if(dobString != null) {
-				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+				System.out.println("dob string ::"+dobString);
+				//DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 				DateTimeFormatter assemblyDateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-				LocalDate date = LocalDate.parse(dobString, formatter);
-				String formattedDOBString = assemblyDateFormatter.format(date);			
-				personalInfoObj.put("dob", formattedDOBString);
+				
+				LocalDate dobDate = (LocalDate) dobString;
+				String formattedDOBString = assemblyDateFormatter.format(dobDate);
+				System.out.println("another formatted dob ::"+formattedDOBString);
+				
+				personalInfoObj.put("dob", formattedDOBString.toString());
 			}
 			
 			if(email != null) {
-				contactInfoObj.put("email", email);
+				contactInfoObj.put("email", email.toString());
 			}
 			
 			if(phoneNumber != null) {
-				contactInfoObj.put("mobile", phoneNumber);
+				contactInfoObj.put("mobile", phoneNumber.toString());
 			}
 			
 			if(addressLine1 != null) {
-				locationObj.put("addressLine1", addressLine1);
+				locationObj.put("addressLine1", addressLine1.toString());
 			}
 			
 			if(city != null) {
-				locationObj.put("city", city);
+				locationObj.put("city", city.toString());
 			}
 			
 			if(state != null) {
-				locationObj.put("state", state);
+				locationObj.put("state", state.toString());
 			}
 			
 			if(country != null) {
-				locationObj.put("country", country);
+				locationObj.put("country", country.toString());
 			}
 			
 			if(postCode != null) {
-				locationObj.put("postcode", postCode);
+				locationObj.put("postcode", postCode.toString());
 			}
 		}
 	
@@ -276,7 +270,99 @@ public class PaymentUtils {
 		userobj.put("id", assemblyUserId);
 		
 		System.out.println("user obj ::"+userobj);
-		PaymentEndpoint.createAssemblyUser(gson.toJson(userobj), authToken);
+	
+		PaymentEndpoint.createAssemblyUser(gson.toJson(userobj), authToken);		
+	}
+	
+	public static String getPaymentsUser(String assemblyUserId, String authToken){
+		String responseString = PaymentEndpoint.getAssemblyUserById(assemblyUserId, authToken);
+		return responseString;
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	public static String updateUserPersonalInfo(String assemblyUserId, String attributeCode, String value, String authToken) {
+
+		System.out.println("attributeCode ::" + attributeCode + ", value ::" + value);
+		String responseString = null;
+
+		org.json.simple.JSONObject userobj = null;
+		org.json.simple.JSONObject personalInfoObj = null;
+		org.json.simple.JSONObject contactInfoObj = null;
+		org.json.simple.JSONObject locationObj = null;
+
+		switch (attributeCode) {
+		case "PRI_FIRSTNAME":
+			personalInfoObj = new org.json.simple.JSONObject();
+			personalInfoObj.put("firstName", value);
+			break;
+		case "PRI_LASTNAME":
+			personalInfoObj = new org.json.simple.JSONObject();
+			personalInfoObj.put("lastName", value);
+			break;
+		case "PRI_DOB":
+			personalInfoObj = new org.json.simple.JSONObject();
+			DateTimeFormatter assemblyDateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+			LocalDate date = LocalDate.parse(value.toString(), formatter);
+			String formattedDOBString = assemblyDateFormatter.format(date);
+			System.out.println("another formatted dob ::" + formattedDOBString);
+			personalInfoObj.put("dob", formattedDOBString.toString());
+			break;
+		case "PRI_EMAIL":
+			contactInfoObj = new org.json.simple.JSONObject();
+			contactInfoObj.put("email", value);
+			break;
+		case "PRI_MOBILE":
+			contactInfoObj = new org.json.simple.JSONObject();
+			contactInfoObj.put("mobile", value);
+			break;
+		case "PRI_ADDRESS_ADDRESS1":
+			contactInfoObj = new org.json.simple.JSONObject();
+			contactInfoObj.put("addressLine1", value);
+			break;
+		case "PRI_ADDRESS_CITY":
+			locationObj = new org.json.simple.JSONObject();
+			locationObj.put("city", value);
+			break;
+		case "PRI_ADDRESS_STATE":
+			locationObj = new org.json.simple.JSONObject();
+			locationObj.put("state", value);
+			break;
+		case "PRI_ADDRESS_COUNTRY":
+			locationObj = new org.json.simple.JSONObject();
+			locationObj.put("country", value);
+			break;
+		case "PRI_ADDRESS_POSTCODE":
+			locationObj = new org.json.simple.JSONObject();
+			locationObj.put("postcode", value);
+			break;
+		}
+
+		if (personalInfoObj != null) {
+			userobj = new org.json.simple.JSONObject();
+			userobj.put("personalInfo", personalInfoObj);
+			userobj.put("id", assemblyUserId);
+		}
+
+		if (contactInfoObj != null) {
+			userobj = new org.json.simple.JSONObject();
+			userobj.put("contactInfo", contactInfoObj);
+			userobj.put("id", assemblyUserId);
+		}
+
+		if (locationObj != null) {
+			userobj = new org.json.simple.JSONObject();
+			userobj.put("location", locationObj);
+			userobj.put("id", assemblyUserId);
+		}
+		
+		if(userobj != null) {
+			responseString = PaymentEndpoint.updateAssemblyUser(assemblyUserId, gson.toJson(userobj), authToken);
+		}
+		
+		return responseString;
+
 	}
 
 }
