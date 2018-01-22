7e3d68089d40162421682b73ab37fabe265e236d
rpgayatri <rpgayatri@gmail.com>
2018-01-22 17:19:51 +1100

    Adding generic template for triggering messages

--- src/main/java/life/genny/qwandautils/MessageUtils.java
+++ src/main/java/life/genny/qwandautils/MessageUtils.java
@@ -1,12 +1,15 @@
 package life.genny.qwandautils;
 
 import java.lang.invoke.MethodHandles;
+import java.util.Map;
 
 import org.apache.logging.log4j.Logger;
 
 import io.vertx.core.json.JsonObject;
 import life.genny.qwanda.message.QBaseMSGMessageType;
 import life.genny.qwanda.message.QMSGMessage;
+import life.genny.qwanda.message.QMessage;
+import life.genny.qwanda.message.QMessageGennyMSG;
 
 public class MessageUtils {
 	
@@ -54,5 +57,28 @@ public class MessageUtils {
 
 		return jsonMessage;
 	}
+	
+	
+	public static JsonObject prepareMessageTemplate(String templateCode, String messageType, Map<String, String> contextMap, String[] recipientArray, String token ) {
+		
+		QBaseMSGMessageType type = null;
+		if(messageType.equals("SMS")) {
+			type = QBaseMSGMessageType.SMS;
+		}else if(messageType.equals("EMAIL")) {
+			type = QBaseMSGMessageType.EMAIL;
+		}
+		
+		QMessageGennyMSG msgMessage = new QMessageGennyMSG("MSG_MESSAGE", type, templateCode, contextMap, recipientArray);
+		JsonObject jsonMessage = new JsonObject().mapFrom(msgMessage);
+
+		log.info("------------------------------------------------------------------------");
+		log.info("MESSAGE TO OWNER   ::   " + jsonMessage.toString());
+		log.info("------------------------------------------------------------------------");
+
+		jsonMessage.put("token", token);
+
+		return jsonMessage;	
+	}
+	
 
 }
