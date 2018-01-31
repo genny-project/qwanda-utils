12b9a6592069270fe01345f3c1cd991a8ab627df
Adam Crow <acrow@Adams-Compooter.local>
2018-01-31 22:46:44 +1100

    Upgraded to qwanda 0.9.22 and gson consolidation

---
+++ src/main/java/life/genny/qwandautils/JsonUtils.java
@@ -0,0 +1,72 @@
+package life.genny.qwandautils;
+
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.apache.logging.log4j.Logger;
+import org.javamoney.moneta.Money;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+
+import life.genny.qwanda.DateTimeDeserializer;
+import life.genny.qwanda.MoneyDeserializer;
+import life.genny.qwanda.entity.BaseEntity;
+
+public class JsonUtils {
+	
+	protected static final Logger log = org.apache.logging.log4j.LogManager
+			.getLogger(MethodHandles.lookup().lookupClass().getCanonicalName());
+
+	static GsonBuilder gsonBuilder = new GsonBuilder();
+	static public Gson gson = gsonBuilder.registerTypeAdapter(Money.class, new MoneyDeserializer())
+			.registerTypeAdapter(LocalDateTime.class, new DateTimeDeserializer()).setPrettyPrinting()
+			.registerTypeAdapter(LocalDate.class, new JsonDeserializer<LocalDate>() {
+				@Override
+				public LocalDate deserialize(final JsonElement json, final Type type,
+						final JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+					return LocalDate.parse(json.getAsJsonPrimitive().getAsString(), DateTimeFormatter.ISO_LOCAL_DATE);
+				}
+
+				public JsonElement serialize(final LocalDate date, final Type typeOfSrc,
+						final JsonSerializationContext context) {
+					return new JsonPrimitive(date.format(DateTimeFormatter.ISO_LOCAL_DATE));
+				}
+			})
+			// .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
+			.excludeFieldsWithoutExposeAnnotation().create();
+	
+	
+	public static <T> T fromJson(final String json, Class clazz)
+	{
+	        T item = null;
+	        if (json != null) {
+	                try {
+	                	if (clazz.getSimpleName().equalsIgnoreCase(BaseEntity.class.getSimpleName())) {
+	                		 item = (T)gson.fromJson(json, clazz);
+	                	} else {
+	                      item = (T)gson.fromJson(json, clazz);
+	                	}
+	                } catch (Exception e) {
+	                     log.error("Bad Deserialisation for "+clazz.getSimpleName());
+	                }
+	        }
+	        return item;
+	}
+	
+	public static String toJson(Object obj)
+	{
+	
+		String ret =  gson.toJson(obj);
+		return ret;
+	}
+}
