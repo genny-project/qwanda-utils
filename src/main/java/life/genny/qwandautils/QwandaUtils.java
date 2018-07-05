c8466321ffe627ffd92348177ac85da2c9f22a42
Adam Crow <acrow@Adams-Compooter.local>
2018-07-05 10:08:25 +1000

    Updated

--- src/main/java/life/genny/qwandautils/QwandaUtils.java
+++ src/main/java/life/genny/qwandautils/QwandaUtils.java
@@ -320,6 +320,9 @@ public class QwandaUtils {
 	}
 
 	public static String getNormalisedUsername(final String rawUsername) {
+		if (rawUsername==null) {
+			return null;
+		}
 		return rawUsername.replaceAll("\\&", "_AND_").replaceAll("@", "_AT_").replaceAll("\\.", "")
 				.replaceAll("\\+", "_PLUS_").toUpperCase();
 	}
@@ -1383,91 +1386,6 @@ public class QwandaUtils {
 		return null;
 	}
 
-	public static Boolean doesQuestionGroupExist(String sourceCode, String targetCode, final String questionCode, String token) {
-
-		/* we grab the question group using the questionCode */
-		QDataAskMessage questions = QwandaUtils.getAsks(sourceCode, targetCode, questionCode, token);
-
-		/* we check if the question payload is not empty */
-		if (questions != null) {
-
-			/* we check if the question group contains at least one question */
-			if (questions.getItems() != null && questions.getItems().length > 0) {
-
-				Ask firstQuestion = questions.getItems()[0];
-
-				/* we check if the question is a question group */
-				if (firstQuestion.getAttributeCode().contains("QQQ_QUESTION_GROUP_BUTTON_SUBMIT")) {
-
-					/* we see if this group contains at least one question */
-					return firstQuestion.getChildAsks().length > 0;
-				} else {
-
-					/* if it is an ask we return true */
-					return true;
-				}
-			}
-		}
-
-		/* we return false otherwise */
-		return false;
-	}
-
-	private static QDataAskMessage getAsks(String sourceCode, String targetCode, String questionCode, String token) {
-
-		String json;
-		try {
-
-			json = QwandaUtils.apiGet(QwandaUtils.qwandaServiceUrl + "/qwanda/baseentitys/" + sourceCode + "/asks2/"
-					+ questionCode + "/" + targetCode, token);
-			QDataAskMessage msg = JsonUtils.fromJson(json, QDataAskMessage.class);
-			return msg;
-
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		return null;
-	}
-
-	public static QwandaMessage getQuestions(String sourceCode, String targetCode, String questionCode, String token) throws ClientProtocolException, IOException {
-		return QwandaUtils.getQuestions(sourceCode, targetCode, questionCode, token, null, true);
-	}
-
-	public static QwandaMessage getQuestions(String sourceCode, String targetCode, String questionCode, String token, String stakeholderCode, Boolean pushSelection) throws ClientProtocolException, IOException {
-
-		QBulkMessage bulk = new QBulkMessage();
-		QwandaMessage qwandaMessage = new QwandaMessage();
-
-		QDataAskMessage questions = QwandaUtils.getAsks(sourceCode, targetCode, questionCode, token);
-		if (questions != null) {
-
-			/*
-			 * if we have the questions, we loop through the asks and send the required data
-			 * to front end
-			 */
-
-			Ask[] asks = questions.getItems();
-			if (asks != null && pushSelection == true) {
-				QBulkMessage askData = QwandaUtils.sendAsksRequiredData(asks, token, stakeholderCode);
-				for(QDataBaseEntityMessage message: askData.getMessages()) {
-					bulk.add(message);
-				}
-			}
-
-			qwandaMessage.askData = bulk;
-			qwandaMessage.asks = questions;
-
-			return qwandaMessage;
-
-		} else {
-			log.error("Questions Msg is null " + sourceCode + "/asks2/" + questionCode + "/" + targetCode);
-		}
-
-		return null;
-	}
 
 	private static Attribute getAttribute(String attributeCode, String token) {
 
@@ -1488,136 +1406,27 @@ public class QwandaUtils {
 		return null;
 	}
 
-	private static QBulkMessage sendAsksRequiredData(Ask[] asks, String token, String stakeholderCode) {
 
-		QBulkMessage bulk = new QBulkMessage();
-
-		/* we loop through the asks and send the required data if necessary */
-		for (Ask ask : asks) {
-
-			/*
-			 * we get the attribute code. if it starts with "LNK_" it means it is a dropdown
-			 * selection.
-			 */
-
-			String attributeCode = ask.getAttributeCode();
-			if (attributeCode != null && attributeCode.startsWith("LNK_")) {
-
-				/* we get the attribute validation to get the group code */
-				Attribute attribute = QwandaUtils.getAttribute(attributeCode, token);
-				if(attribute != null) {
-
-					/* grab the group in the validation */
-					DataType attributeDataType = attribute.getDataType();
-					if(attributeDataType != null) {
-
-						List<Validation> validations = attributeDataType.getValidationList();
-
-						/* we loop through the validations */
-						for(Validation validation: validations) {
-
-							List<String> validationStrings = validation.getSelectionBaseEntityGroupList();
-
-							for(String validationString: validationStrings) {
-
-								if(validationString.startsWith("GRP_")) {
-
-									/* we have a GRP. we push it to FE */
-									List<BaseEntity> bes = QwandaUtils.getBaseEntityWithChildren(validationString, 2, token);
-									List<BaseEntity> filteredBes = null;
-
-									if(bes != null) {
-										
-										/* hard coding this for now. sorry */
-										if(attributeCode.equals("LNK_LOAD_LISTS") && stakeholderCode != null) {
-
-											/* we filter load you only are a stakeholder of */
-											filteredBes = bes.stream().filter(baseEntity -> {
-												return baseEntity.getValue("PRI_AUTHOR", "").equals(stakeholderCode);
-											}).collect(Collectors.toList());
-										}
-										else {
-											filteredBes = bes;
-										}
-
-										QDataBaseEntityMessage beMessage = new QDataBaseEntityMessage(filteredBes);
-										beMessage.setLinkCode("LNK_CORE");
-										beMessage.setParentCode(validationString);
-										beMessage.setReplace(true);
-										bulk.add(beMessage);
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-
-			/* recursive call */
-			Ask[] childAsks = ask.getChildAsks();
-			if (childAsks != null && childAsks.length > 0) {
-
-				QBulkMessage newBulk = QwandaUtils.sendAsksRequiredData(childAsks, token, stakeholderCode);
-				for(QDataBaseEntityMessage msg: newBulk.getMessages()) {
-					bulk.add(msg);
-				}
-			}
-		}
-
-		return bulk;
+	/**
+	 * @return the apiPort
+	 */
+	public static int getApiPort() {
+		return apiPort;
 	}
 
-	public static QwandaMessage askQuestions(final String sourceCode, final String targetCode, final String questionGroupCode, String token) {
-		return QwandaUtils.askQuestions(sourceCode, targetCode, questionGroupCode, token, null, true);
-	}
-	
-	public static QwandaMessage askQuestions(final String sourceCode, final String targetCode, final String questionGroupCode, String token, String stakeholderCode) {
-		return QwandaUtils.askQuestions(sourceCode, targetCode, questionGroupCode, token, stakeholderCode, true);
-	}
-	
-	public static QwandaMessage askQuestions(final String sourceCode, final String targetCode, final String questionGroupCode, Boolean pushSelection) {
-		return QwandaUtils.askQuestions(sourceCode, targetCode, questionGroupCode, null, null, pushSelection);
-	}
-	
-	public static QwandaMessage askQuestions(final String sourceCode, final String targetCode, final String questionGroupCode, String token, Boolean pushSelection) {
-		return QwandaUtils.askQuestions(sourceCode, targetCode, questionGroupCode, token, null, pushSelection);
+	/**
+	 * @return the hostIP
+	 */
+	public static String getHostIP() {
+		return hostIP;
 	}
 
-	public static QwandaMessage askQuestions(final String sourceCode, final String targetCode, final String questionGroupCode, final String token, final String stakeholderCode, final Boolean pushSelection) {
-
-		try {
-
-			/* if sending the questions worked, we ask user */
-			return QwandaUtils.getQuestions(sourceCode, targetCode, questionGroupCode, token, stakeholderCode, pushSelection);
-
-		} catch (Exception e) {
-			System.out.println("Ask questions exception: ");
-			e.printStackTrace();
-			return null;
-		}
+	/**
+	 * @return the qwandaServiceUrl
+	 */
+	public static String getQwandaServiceUrl() {
+		return qwandaServiceUrl;
 	}
-	
-	public static QwandaMessage setCustomQuestion(QwandaMessage questions, String questionAttributeCode, String customTemporaryQuestion) {
-		
-		if(questions != null && questionAttributeCode != null) {
-			Ask[] askArr = questions.asks.getItems();
-			if(askArr != null && askArr.length > 0) {
-				for(Ask ask : askArr) {
-					Ask[] childAskArr = ask.getChildAsks();
-					if(childAskArr != null && childAskArr.length > 0) {
-						for(Ask childAsk : childAskArr) {
-							System.out.println("child ask code :: "+childAsk.getAttributeCode() + ", child ask name :: "+childAsk.getName());
-							if(childAsk.getAttributeCode().equals(questionAttributeCode)) {
-								if(customTemporaryQuestion != null) {
-									childAsk.getQuestion().setName(customTemporaryQuestion);
-									return questions;
-								}								
-							}
-						}
-					}
-				}
-			}	
-		}
-		return questions;
-	}
+
+
 }
--- src/test/java/life/genny/SecurityUtilsTest.java
+++ src/test/java/life/genny/SecurityUtilsTest.java
@@ -8,7 +8,7 @@ public class SecurityUtilsTest {
 	@Test
 	public void versionTest()
 	{
-		        String key        = "ImAKeythatweuse!"; // 128 bit key
+		        String key        = "WubbaLubbaDubDub"; // 128 bit key
 		        String initVector = "PRJ_GENNY*******"; // 16 bytes IV
 
 		        String encrypted = SecurityUtils.encrypt(key, initVector, "Yo!Dude!IamapublicPassword");
