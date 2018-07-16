b6b6c5766e7d7b4654e1a25d05f9503dc5228408
Adam Crow <acrow@Adams-Compooter.local>
2018-07-16 13:22:22 +1000

    Added common settings file

---
+++ src/main/java/life/genny/qwandautils/GennySettings.java
@@ -0,0 +1,9 @@
+package life.genny.qwandautils;
+
+public class GennySettings {
+	public static final String qwandaServiceUrl = System.getenv("REACT_APP_QWANDA_API_URL");
+	public static final Boolean devMode = ("TRUE".equalsIgnoreCase(System.getenv("DEV_MODE"))||"TRUE".equalsIgnoreCase(System.getenv("GENNYDEV"))) ? true : false;
+	public static final String projectUrl = System.getenv("PROJECT_URL");
+	public final static String mainrealm = System.getenv("PROJECT_REALM"); // UGLY
+
+}
