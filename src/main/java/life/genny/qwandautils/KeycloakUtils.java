78b05827ae879f6e578fda498af6c7408f4f94ff
Adam Crow <acrow@Adams-Compooter.local>
2018-07-16 14:25:21 +1000

    Added encryption and decryption functions to keycloak

--- src/main/java/life/genny/qwandautils/KeycloakUtils.java
+++ src/main/java/life/genny/qwandautils/KeycloakUtils.java
@@ -13,6 +13,10 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.Header;
@@ -458,4 +462,60 @@ public class KeycloakUtils {
 
 		return null;
 	}
+	
+	public String createEncryptedPassword(String key, final String customercode, final String password) {
+		String newkey = null;
+		if (key.length()>16) {
+			newkey = key.substring(0, 16);
+		} else {
+			newkey = StringUtils.rightPad(key, 16, '*');
+		}
+		String initVector = "PRJ_" + customercode.toUpperCase();
+		initVector = StringUtils.rightPad(initVector, 16, '*');
+
+        String encryptedPassword = encrypt(newkey, initVector, password);
+        if (!key.equals(newkey)) {
+        	System.out.println("NEW KEY = ["+newkey+"]");;
+        }
+		return encryptedPassword;
+	}
+
+    public static String encrypt(String key, String initVector, String value) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+            byte[] encrypted = cipher.doFinal(value.getBytes());
+//            System.out.println("encrypted string: "
+//                    + Base64.encodeBase64String(encrypted));
+
+            return Base64.encodeBase64String(encrypted);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static String decrypt(String key, String initVector, String encrypted) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+
+            byte[] original = cipher.doFinal(Base64.decodeBase64(encrypted));
+
+            return new String(original);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+
 }
