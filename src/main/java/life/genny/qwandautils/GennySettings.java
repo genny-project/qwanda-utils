95e1648e08ddb16e28ca03d5ef1e1c8ed1ab1ccc
Adam Crow <acrow@Adams-Compooter.local>
2018-07-16 13:26:20 +1000

    Added isRulesManager

--- src/main/java/life/genny/qwandautils/GennySettings.java
+++ src/main/java/life/genny/qwandautils/GennySettings.java
@@ -5,5 +5,6 @@ public class GennySettings {
 	public static final Boolean devMode = ("TRUE".equalsIgnoreCase(System.getenv("DEV_MODE"))||"TRUE".equalsIgnoreCase(System.getenv("GENNYDEV"))) ? true : false;
 	public static final String projectUrl = System.getenv("PROJECT_URL");
 	public final static String mainrealm = System.getenv("PROJECT_REALM"); // UGLY
+	public final static Boolean isRulesManager = "TRUE".equalsIgnoreCase(System.getenv("RULESMANAGER"));
 
 }
