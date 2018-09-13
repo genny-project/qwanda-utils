aebd4c821ebcbd25280f9e1174f4b8d9f6eca020
jayavani swaminathan <jayavaniswaminathan@jayavanis-MacBook-Pro.local>
2018-09-13 15:07:44 +1000

    Fixed Git Version Issue

--- src/main/java/life/genny/qwandautils/GitUtils.java
+++ src/main/java/life/genny/qwandautils/GitUtils.java
@@ -1,6 +1,8 @@
 package life.genny.qwandautils;
 
 import org.apache.http.client.ClientProtocolException;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.Model;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.InvalidRemoteException;
@@ -14,14 +16,21 @@ import org.eclipse.jgit.revwalk.RevTree;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.treewalk.TreeWalk;
 import org.eclipse.jgit.treewalk.filter.PathFilter;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 public class GitUtils {
   public static String gitGet(final String branch, final String project,
-      final String repositoryName, final String layoutFilename) throws ClientProtocolException,
-      IOException, InvalidRemoteException, TransportException, GitAPIException {
+      final String repositoryName, final String layoutFilename) throws
+      IOException, GitAPIException {
     String retJson = "";
 
     final Git git = Git.cloneRepository()
@@ -62,6 +71,36 @@ public class GitUtils {
     }
     return retJson;
   }
-
-
+  
+  public static String getGitVersionString(Model model) throws IOException {
+    Properties properties;
+    JsonObject response = new JsonObject();
+    JsonArray versionArray = new JsonArray();
+    Map<String,Object> versionMap;
+    
+    String projectPropertyFileName = model.getArtifactId() + "-git.properties";
+    URL fileURL = Thread.currentThread().getContextClassLoader().getResource(projectPropertyFileName);
+    if(fileURL != null && fileURL.getFile() != "") {
+      properties = new Properties();
+      properties.load(fileURL.openStream());
+      versionMap = new HashMap<>();
+      versionMap.put(model.getArtifactId(), new JsonObject(JsonUtils.toJson(properties)));
+      versionArray.add(versionMap);
+    }
+    
+    List<Dependency> dependencyList = model.getDependencies();
+    for(Dependency dependency : dependencyList) {
+      String dependencyProjectFileName = dependency.getArtifactId() + "-git.properties";
+      URL dependencyURL = Thread.currentThread().getContextClassLoader().getResource(dependencyProjectFileName);
+      if(dependencyURL != null && dependencyURL.getFile() != "") {
+        properties = new Properties();
+        properties.load(dependencyURL.openStream());
+        versionMap = new HashMap<>();
+        versionMap.put(dependency.getArtifactId(), new JsonObject(JsonUtils.toJson(properties)));
+        versionArray.add(versionMap);
+      }
+    }
+    response.put("version", versionArray);
+    return response.toString();
+  }
 }
