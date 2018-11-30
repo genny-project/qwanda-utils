92ef3f0fb90ae56047db20e771c8c00e8034bfa1
rpgayatri <rpgayatri@gmail.com>
2018-11-30 15:59:02 +1100

    new storage util

--- src/main/java/life/genny/qwandautils/StorageUtils.java
+++ src/main/java/life/genny/qwandautils/StorageUtils.java
@@ -4,26 +4,32 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.invoke.MethodHandles;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.UUID;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.logging.log4j.Logger;
 
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.SdkClientException;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.s3.model.S3Object;
 
 import life.genny.qwanda.FileUploadDetails;
 import life.genny.qwanda.entity.BaseEntity;
 
 public class StorageUtils {
+	
+	protected static final Logger log = org.apache.logging.log4j.LogManager
+			.getLogger(MethodHandles.lookup().lookupClass().getCanonicalName());
 	
 	private static AmazonS3 getAmazonClientObj(BaseEntity projectBe) {
 		
@@ -32,11 +38,10 @@ public class StorageUtils {
 		String clientRegion = projectBe.getValue("PRI_AWS_S3_STORAGE_CLIENT_REGION", null);
 		
 		BasicAWSCredentials awsCreds = new BasicAWSCredentials(key, secret);
-		AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+		
+		return AmazonS3ClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(awsCreds)).withRegion(clientRegion)
                 .build();
-		
-		return s3Client;
 		
 	}
 	
@@ -44,82 +49,100 @@ public class StorageUtils {
 	public static FileUploadDetails uploadToAWSS3(String downloadableUrl, FileUploadDetails fileUploadDetails, BaseEntity projectBe, String mimeType) {	
 		
 		AmazonS3 s3Client = getAmazonClientObj(projectBe);
+		FileUploadDetails uploadDetails = new FileUploadDetails();
 	
 		String bucketName = projectBe.getValue("PRI_AWS_S3_STORAGE_BUCKET_NAME", null);
-		PutObjectRequest request = null;
-		ObjectMetadata metadata = new ObjectMetadata();
 		
 		if(bucketName != null && s3Client != null) {
+			
+			/* unique ID for file */
+			String fileUUID = UUID.randomUUID().toString();
+			log.info("fileUUID ::"+fileUUID);
+			
+			/* create a new file */
+			File file = new File("/tmp/"+fileUUID);
+			
+			log.info("file ::"+file.getName());
+			log.info("file path ::"+file.getAbsolutePath());
+			
+			/* get content from URL and convert to inputstream */
+			URL url;
 			try {
+				url = new URL(downloadableUrl);
 
-	            // Upload a text string as a new object.
-				s3Client.putObject(bucketName, fileUploadDetails.getUploadObjKey(), fileUploadDetails.getUploadObjValue());
+				uploadDetails = uploadAndGetDetails(s3Client, bucketName, url, file, fileUUID, fileUploadDetails, mimeType);
 				
-				/* unique ID for file */
-				String fileUUID = UUID.randomUUID().toString();
-				System.out.println("fileUUID ::"+fileUUID);
 				
-				/* get content from URL and convert to inputstream */
-				URL url = new URL(downloadableUrl);
-				InputStream is = url.openStream();
-				
-				/* create a new file */
-				File file = new File("/tmp/"+fileUUID);
-				
-				/* copying contents to file outputstream */
-				OutputStream outputStream = new FileOutputStream(file);
-				IOUtils.copy(is, outputStream);
-				
-				System.out.println("file ::"+file.getName());
-				System.out.println("file path ::"+file.getAbsolutePath());
-					
-				// Upload a file as a new object with ContentType and title specified.
-				
-				if(fileUploadDetails.getFileName() != null) {
-					request = new PutObjectRequest(bucketName, fileUploadDetails.getFileName(), file);
-					fileUploadDetails.setFileName(fileUploadDetails.getFileName());
-				} else {
-					request = new PutObjectRequest(bucketName, fileUUID, file);
-					fileUploadDetails.setFileName(fileUUID);
-				}		
-				
-				/* Finally uploads to S3 bucket */
-	            metadata.setContentType(mimeType);
-				metadata.addUserMetadata(fileUploadDetails.getUserMetaDataObjKey(), fileUploadDetails.getUserMetaDataObjValue());
-	            request.setMetadata(metadata);
-	            request.withCannedAcl(CannedAccessControlList.PublicRead);
-	            PutObjectResult result = s3Client.putObject(request);
-	            
-	            String uploadedUrl = String.valueOf(s3Client.getUrl(bucketName, file.getName()));
-	                        
-	            System.out.println("uploaded url ::"+uploadedUrl);
-	            fileUploadDetails.setUploadedFilePath(uploadedUrl);
-	            
-				is.close();
-				outputStream.close();
-	            
-	        }
-	        catch(AmazonServiceException e) {
-	            // The call was transmitted successfully, but Amazon S3 couldn't process 
-	            // it, so it returned an error response.
-	            e.printStackTrace();
-	        }
-	        catch(SdkClientException e) {
-	            // Amazon S3 couldn't be contacted for a response, or the client
-	            // couldn't parse the response from Amazon S3.
-	            e.printStackTrace();
-	        } catch (IOException e) {
-				e.printStackTrace();
+			} catch (MalformedURLException e1) {
+				 log.error(e1.getMessage());
 			}
+			 
 		}
-		
-		
-		System.out.println("fileUploadDetails ::"+fileUploadDetails);
-		return fileUploadDetails;
+
+		log.info("fileUploadDetails ::"+uploadDetails);
+		return uploadDetails;
 
 	}
 	
-	
+	/**
+	 * 
+	 * @param s3Client
+	 * @param bucketName
+	 * @param url of file
+	 * @param file
+	 * @param fileUUID UUID that we generate
+	 * @param fileUploadDetails Object to store details of file upload
+	 * @param mimeType Mimetype of the file being uploaded
+	 * @return
+	 */
+	private static FileUploadDetails uploadAndGetDetails(AmazonS3 s3Client, String bucketName, URL url, File file, String fileUUID,
+			FileUploadDetails fileUploadDetails, String mimeType) {
+		
+		PutObjectRequest request = null;
+		ObjectMetadata metadata = new ObjectMetadata();
+
+		/* copying contents to file outputstream */
+		/* using try with resources */
+		try (InputStream is = url.openStream(); OutputStream outputStream = new FileOutputStream(file)) {
+
+            // Upload a text string as a new object.
+			s3Client.putObject(bucketName, fileUploadDetails.getUploadObjKey(), fileUploadDetails.getUploadObjValue());
+
+			IOUtils.copy(is, outputStream);
+
+			// Upload a file as a new object with ContentType and title specified.
+			
+			if(fileUploadDetails.getFileName() != null) {
+				request = new PutObjectRequest(bucketName, fileUploadDetails.getFileName(), file);
+				fileUploadDetails.setFileName(fileUploadDetails.getFileName());
+			} else {
+				request = new PutObjectRequest(bucketName, fileUUID, file);
+				fileUploadDetails.setFileName(fileUUID);
+			}		
+			
+			/* Finally uploads to S3 bucket */
+            metadata.setContentType(mimeType);
+			metadata.addUserMetadata(fileUploadDetails.getUserMetaDataObjKey(), fileUploadDetails.getUserMetaDataObjValue());
+            request.setMetadata(metadata);
+            request.withCannedAcl(CannedAccessControlList.PublicRead);
+            
+            String uploadedUrl = String.valueOf(s3Client.getUrl(bucketName, file.getName()));
+                        
+            log.info("uploaded url ::"+uploadedUrl);
+            fileUploadDetails.setUploadedFilePath(uploadedUrl);	
+           
+            return fileUploadDetails;
+            
+        }
+        catch(SdkClientException | IOException e) {
+            // The call was transmitted successfully, but Amazon S3 couldn't process 
+            // it, so it returned an error response.
+            log.error(e.getMessage());
+        }
+		return null;
+	}
+
+
 	public static String getUploadedS3Link(String fileName, BaseEntity projectBe) {
 		
 		String uploadedUrl = null;
@@ -131,11 +154,23 @@ public class StorageUtils {
 	                bucketName, 
 	                fileName));
 	                     
-	         System.out.println("uploaded url ::"+uploadedUrl);
+			log.info("uploaded url ::"+uploadedUrl);
 		}
 		
 		return uploadedUrl;
 		
+	}
+	
+	public static InputStream getObject(BaseEntity projectBe, String key) {
+		
+		AmazonS3 s3Client = getAmazonClientObj(projectBe);
+		String bucketName = projectBe.getValue("PRI_AWS_S3_STORAGE_BUCKET_NAME", null);
+		
+		if(s3Client != null && bucketName != null) {
+			S3Object object = s3Client.getObject(new GetObjectRequest(bucketName, key));
+			return object.getObjectContent();
+		}
+		return null;
 	}
 
 }
