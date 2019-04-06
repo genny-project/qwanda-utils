edc3abd858504477e817ea824a20648fc0eae04b
Adam Crow <adamcrow63@gmail.com>
2019-04-06 12:51:33 +1100

    Added ABN lookup utils

---
+++ src/main/java/life/genny/models/AbnSearchResult.java
@@ -0,0 +1,34 @@
+package life.genny.models;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.w3c.dom.Node;
+
+import life.genny.qwandautils.XMLUtils;
+
+public class AbnSearchResult
+{
+        private Node    searchResultsPayload;
+
+        public AbnSearchResult(Node searchResultsPayload)
+        {
+                this.searchResultsPayload = searchResultsPayload;
+        }
+
+        public boolean isException()
+        {
+                return XMLUtils.getNode(searchResultsPayload, "/ABRPayloadSearchResults/response/exception") != null;
+        }
+
+        public String getOrganisationName() throws SecurityException, IllegalArgumentException, ClassNotFoundException, NoSuchMethodException,
+                        IllegalAccessException, InvocationTargetException
+        {
+                return XMLUtils.getNodeText(searchResultsPayload, "/ABRPayloadSearchResults/response/businessEntity/mainName/organisationName");
+        }
+
+        public String getExceptionDescription() throws SecurityException, IllegalArgumentException, ClassNotFoundException, NoSuchMethodException,
+                        IllegalAccessException, InvocationTargetException
+        {
+                return XMLUtils.getNodeText(searchResultsPayload, "/ABRPayloadSearchResults/response/exception/exceptionDescription");
+        }
+}
