06a73dd93fdf8ee036f871694f398cc3631cb78d
Adam Crow <adamcrow63@gmail.com>
2019-06-19 00:58:42 +1000

    Fixed up bulk Ask message

--- src/main/java/life/genny/qwandautils/QwandaMessage.java
+++ src/main/java/life/genny/qwandautils/QwandaMessage.java
@@ -1,10 +1,33 @@
 package life.genny.qwandautils;
 
+import java.io.Serializable;
+
+import com.google.gson.annotations.Expose;
+
 import life.genny.qwanda.message.QBulkMessage;
+import life.genny.qwanda.message.QCmdMessage;
 import life.genny.qwanda.message.QDataAskMessage;
 
-public class QwandaMessage {
+public class QwandaMessage extends  QCmdMessage implements Serializable {
+		
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	
+	@Expose
 	public QDataAskMessage asks;
+	@Expose
 	public QBulkMessage askData;
+	
+	private static final String CMD_TYPE = "CMD_BULKASK";
+	private static final String CODE = "QWANDAMESSAGE";
+
+	
+	public QwandaMessage() {
+		super(CMD_TYPE, CODE);
+		
+	}
+
+	
 }
