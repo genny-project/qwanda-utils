9e145c0837f0129e0c79a268f1336d750f743edd
Adam Crow <adamcrow63@gmail.com>
2019-09-02 15:38:18 +1000

    Updated saving of frames, asks to not changeEvent

--- src/main/java/life/genny/models/GennyToken.java
+++ src/main/java/life/genny/models/GennyToken.java
@@ -1,6 +1,7 @@
 package life.genny.models;
 
 import java.io.Serializable;
+import java.lang.invoke.MethodHandles;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
@@ -19,11 +20,13 @@ import java.util.regex.Pattern;
 import javax.persistence.Transient;
 import javax.xml.bind.annotation.XmlTransient;
 
+import org.apache.logging.log4j.Logger;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import life.genny.qwanda.exception.BadDataException;
 import life.genny.qwandautils.KeycloakUtils;
 import life.genny.qwandautils.QwandaUtils;
 import life.genny.qwandautils.SecurityUtils;
@@ -33,6 +36,9 @@ public class GennyToken implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	protected static final Logger log = org.apache.logging.log4j.LogManager
+			.getLogger(MethodHandles.lookup().lookupClass().getCanonicalName());
+
 	String code;
 	String userCode;
 	String token;
@@ -44,47 +50,50 @@ public class GennyToken implements Serializable {
 		if ((token != null) && (!token.isEmpty())) {
 			// Getting decoded token in Hash Map from QwandaUtils
 			adecodedTokenMap = KeycloakUtils.getJsonMap(token);
+			if (adecodedTokenMap == null) {
+				log.error("Token is not able to be decoded in GennyToken ..");
+			} else {
 
-			// Extracting realm name from iss value
+				// Extracting realm name from iss value
 
-			String realm = (adecodedTokenMap.get("azp").toString());
-			if (realm == null) {
-				realm = (adecodedTokenMap.get("aud").toString());  // handle non Keycloak 6+
+				String realm = (adecodedTokenMap.get("azp").toString());
+				if (realm == null) {
+					realm = (adecodedTokenMap.get("aud").toString()); // handle non Keycloak 6+
+				}
+
+				// Adding realm name to the decoded token
+				adecodedTokenMap.put("realm", realm);
+				this.token = token;
+				this.realm = realm;
+				String username = (String) adecodedTokenMap.get("preferred_username");
+				String normalisedUsername = QwandaUtils.getNormalisedUsername(username);
+				this.userCode = "PER_" + normalisedUsername.toUpperCase();
+
+				setupRoles();
 			}
 
-			// Adding realm name to the decoded token
-			adecodedTokenMap.put("realm", realm);
-			this.token = token;
-			this.realm = realm;
-			String username = (String)adecodedTokenMap.get("preferred_username");
-			String normalisedUsername = QwandaUtils.getNormalisedUsername(username);
-			this.userCode = "PER_"+normalisedUsername.toUpperCase();
-
-			setupRoles();
-			
-			
+		} else {
+			log.error("Token is null or zero length in GennyToken ..");
 		}
-		
-	}
-	
-	
-	
-	public GennyToken(final String code,final String token) {
 
-			this(token);
-			this.code = code;
 	}
 
-	
-	public GennyToken(final String code, final String id, final String issuer, final String subject, final long ttl, final String secret,
-			final String realm, final String username, final String name, final String role) {
-		
-		this(code,id,issuer,subject, ttl,secret,realm,username, name, role,LocalDateTime.now().plusSeconds(24*60*60)); // 1 day expiry
+	public GennyToken(final String code, final String token) {
+
+		this(token);
+		this.code = code;
 	}
-	
-	
-	public GennyToken(final String code, final String id, final String issuer, final String subject, final long ttl, final String secret,
-			final String realm, final String username, final String name, final String role, final LocalDateTime expiryDateTime) {
+
+	public GennyToken(final String code, final String id, final String issuer, final String subject, final long ttl,
+			final String secret, final String realm, final String username, final String name, final String role) {
+
+		this(code, id, issuer, subject, ttl, secret, realm, username, name, role,
+				LocalDateTime.now().plusSeconds(24 * 60 * 60)); // 1 day expiry
+	}
+
+	public GennyToken(final String code, final String id, final String issuer, final String subject, final long ttl,
+			final String secret, final String realm, final String username, final String name, final String role,
+			final LocalDateTime expiryDateTime) {
 		adecodedTokenMap = new HashMap<String, Object>();
 		adecodedTokenMap.put("preferred_username", username);
 		adecodedTokenMap.put("name", name);
@@ -93,10 +102,10 @@ public class GennyToken implements Serializable {
 		adecodedTokenMap.put("aud", realm);
 		adecodedTokenMap.put("realm_access", "[user," + role + "]");
 		adecodedTokenMap.put("exp", expiryDateTime.atZone(ZoneId.of("UTC")).toEpochSecond());
-		adecodedTokenMap.put("iat", LocalDateTime.now().atZone(ZoneId.of("UTC")).toEpochSecond());		
+		adecodedTokenMap.put("iat", LocalDateTime.now().atZone(ZoneId.of("UTC")).toEpochSecond());
 		adecodedTokenMap.put("auth_time", LocalDateTime.now().atZone(ZoneId.of("UTC")).toEpochSecond());
-		adecodedTokenMap.put("session_state", UUID.randomUUID().toString().substring(0, 32)); // TODO set size ot same as keycloak
-		
+		adecodedTokenMap.put("session_state", UUID.randomUUID().toString().substring(0, 32)); // TODO set size ot same
+																								// as keycloak
 
 		userRoles = new HashSet<String>();
 		userRoles.add("user");
@@ -110,25 +119,27 @@ public class GennyToken implements Serializable {
 		token = jwtToken;
 		this.realm = realm;
 		String normalisedUsername = QwandaUtils.getNormalisedUsername(username);
-		this.userCode = "PER_"+normalisedUsername.toUpperCase();
+		this.userCode = "PER_" + normalisedUsername.toUpperCase();
 
 		this.code = code;
 		setupRoles();
 	}
 
-	public GennyToken(final String code,final String realm, final String username, final String name, final String role) {
-		this(code,"ABBCD", "Genny Project", "Test JWT", 100000, "IamASecret", realm, username, name, role,LocalDateTime.now().plusSeconds(24*60*60));
+	public GennyToken(final String code, final String realm, final String username, final String name,
+			final String role) {
+		this(code, "ABBCD", "Genny Project", "Test JWT", 100000, "IamASecret", realm, username, name, role,
+				LocalDateTime.now().plusSeconds(24 * 60 * 60));
 	}
 
-	public GennyToken(final String code,final String realm, final String username, final String name, final String role,LocalDateTime expiryDateTime) {
-		this(code,"ABBCD", "Genny Project", "Test JWT", 100000, "IamASecret", realm, username, name, role,expiryDateTime);
+	public GennyToken(final String code, final String realm, final String username, final String name,
+			final String role, LocalDateTime expiryDateTime) {
+		this(code, "ABBCD", "Genny Project", "Test JWT", 100000, "IamASecret", realm, username, name, role,
+				expiryDateTime);
 	}
-
 
 	public String getToken() {
 		return token;
 	}
-
 
 	public Map<String, Object> getAdecodedTokenMap() {
 		return adecodedTokenMap;
@@ -138,54 +149,47 @@ public class GennyToken implements Serializable {
 		this.adecodedTokenMap = adecodedTokenMap;
 	}
 
-	private void setupRoles()
-	{
+	private void setupRoles() {
 		String realm_accessStr = adecodedTokenMap.get("realm_access").toString();
 		Pattern p = Pattern.compile("(?<=\\[)([^\\]]+)(?=\\])");
 		Matcher m = p.matcher(realm_accessStr);
 
 		if (m.find()) {
 			String[] roles = m.group(1).split(",");
-			for (String role : roles) {userRoles.add((String)role.trim()); };
+			for (String role : roles) {
+				userRoles.add((String) role.trim());
+			}
+			;
 		}
 
-
 	}
-	
-	public boolean hasRole(final String role)
-	{
+
+	public boolean hasRole(final String role) {
 		return userRoles.contains(role);
 	}
-	
-	
-	
+
 	@Override
 	public String toString() {
-		return getRealm()+": "+getCode()+": "+getUserCode()+": "+this.userRoles;
+		return getRealm() + ": " + getCode() + ": " + getUserCode() + ": " + this.userRoles;
 	}
-	
-	public String getRealm()
-	{
+
+	public String getRealm() {
 		return realm;
 	}
-	
-	public String getString(final String key)
-	{
-		return (String)adecodedTokenMap.get(key);
+
+	public String getString(final String key) {
+		return (String) adecodedTokenMap.get(key);
 	}
-	
-	public String getCode()
-	{
+
+	public String getCode() {
 		return code;
 	}
 
-	public String getSessionCode()
-	{
+	public String getSessionCode() {
 		return getString("session_state");
 	}
-	
-	public String getClientCode()
-	{
+
+	public String getClientCode() {
 		return getString("aud");
 	}
 
@@ -198,64 +202,50 @@ public class GennyToken implements Serializable {
 
 	@XmlTransient
 	@Transient
-	public LocalDateTime getAuthDateTime()
-	{
-		Long auth_timestamp = ((Number)adecodedTokenMap.get("auth_time")).longValue();
-		LocalDateTime authTime =
-			       LocalDateTime.ofInstant(Instant.ofEpochSecond(auth_timestamp),
-			                               TimeZone.getDefault().toZoneId());
+	public LocalDateTime getAuthDateTime() {
+		Long auth_timestamp = ((Number) adecodedTokenMap.get("auth_time")).longValue();
+		LocalDateTime authTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(auth_timestamp),
+				TimeZone.getDefault().toZoneId());
 		return authTime;
 	}
-	
+
 	@XmlTransient
 	@Transient
-	public LocalDateTime getExpiryDateTime()
-	{
-		Long exp_timestamp = ((Number)adecodedTokenMap.get("exp")).longValue();
-		LocalDateTime expTime =
-			       LocalDateTime.ofInstant(Instant.ofEpochSecond(exp_timestamp),
-			                               TimeZone.getDefault().toZoneId());
+	public LocalDateTime getExpiryDateTime() {
+		Long exp_timestamp = ((Number) adecodedTokenMap.get("exp")).longValue();
+		LocalDateTime expTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(exp_timestamp),
+				TimeZone.getDefault().toZoneId());
 		return expTime;
 	}
-	
-	
+
 	@XmlTransient
 	@Transient
 	public OffsetDateTime getExpiryDateTimeInUTC() {
-				
-		Long exp_timestamp = ((Number)adecodedTokenMap.get("exp")).longValue();
-		LocalDateTime expTime =
-			       LocalDateTime.ofInstant(Instant.ofEpochSecond(exp_timestamp),
-			                               TimeZone.getDefault().toZoneId());
+
+		Long exp_timestamp = ((Number) adecodedTokenMap.get("exp")).longValue();
+		LocalDateTime expTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(exp_timestamp),
+				TimeZone.getDefault().toZoneId());
 		ZonedDateTime ldtZoned = expTime.atZone(ZoneId.systemDefault());
 		ZonedDateTime utcZoned = ldtZoned.withZoneSameInstant(ZoneId.of("UTC"));
-		
+
 		return utcZoned.toOffsetDateTime();
 	}
-	
-	
+
 	// JWT Issue DateTime
 	@XmlTransient
 	@Transient
-	public LocalDateTime getiatDateTime()
-	{
-		Long iat_timestamp = ((Number)adecodedTokenMap.get("iat")).longValue();
-		LocalDateTime iatTime =
-			       LocalDateTime.ofInstant(Instant.ofEpochSecond(iat_timestamp),
-			                               TimeZone.getDefault().toZoneId());
+	public LocalDateTime getiatDateTime() {
+		Long iat_timestamp = ((Number) adecodedTokenMap.get("iat")).longValue();
+		LocalDateTime iatTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(iat_timestamp),
+				TimeZone.getDefault().toZoneId());
 		return iatTime;
 	}
 
-	
-	// Unique token  id
+	// Unique token id
 	@XmlTransient
 	@Transient
-	public String getUniqueId()
-	{
-		return  (String)adecodedTokenMap.get("jti");
+	public String getUniqueId() {
+		return (String) adecodedTokenMap.get("jti");
 	}
-
-
-	
 
 }
