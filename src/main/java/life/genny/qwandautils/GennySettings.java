5159983d5e88619a25b444204bb824b37ea173c3
Adam Crow <adamcrow63@gmail.com>
2020-01-14 11:25:39 +1100

    Default rules to not check hash

--- src/main/java/life/genny/qwandautils/GennySettings.java
+++ src/main/java/life/genny/qwandautils/GennySettings.java
@@ -59,7 +59,7 @@ public class GennySettings {
 	public final static Boolean useSingleton = System.getenv("USE_SINGLETON") !=null ? "TRUE".equalsIgnoreCase(System.getenv("USE_SINGLETON")):true;
 	public final static Boolean useExecutor = System.getenv("USE_EXECUTOR") !=null ? "TRUE".equalsIgnoreCase(System.getenv("USE_EXECUTOR")):true;
 	public final static Boolean useJMS = System.getenv("USE_JMS") !=null ? "TRUE".equalsIgnoreCase(System.getenv("USE_JMS")):false;
-	public final static Boolean detectRuleChanges = System.getenv("DETECT_RULE_CHANGES") !=null ?"TRUE".equalsIgnoreCase(System.getenv("DETECT_RULE_CHANGES")):true;
+	public final static Boolean detectRuleChanges = System.getenv("DETECT_RULE_CHANGES") !=null ?"TRUE".equalsIgnoreCase(System.getenv("DETECT_RULE_CHANGES")):false;
 	public final static Boolean persistRules = System.getenv("PERSIST_RULES") !=null ?"TRUE".equalsIgnoreCase(System.getenv("PERSIST_RULES")):false;
 	public final static Boolean framesOnDemand= System.getenv("FRAMES_ON_DEMAND") !=null ? "TRUE".equalsIgnoreCase(System.getenv("FRAMES_ON_DEMAND")):true;
 	
