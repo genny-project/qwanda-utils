2741002706357432794847e12ecd86aaf77b864c
Jian Li <hbjoylee@gmail.com>
2020-03-05 12:05:12 +1100

    Disable print long json string

--- src/main/java/life/genny/qwandautils/JsonUtils.java
+++ src/main/java/life/genny/qwandautils/JsonUtils.java
@@ -1,32 +1,20 @@
 package life.genny.qwandautils;
 
-import java.lang.invoke.MethodHandles;
-import java.lang.reflect.Modifier;
-import java.lang.reflect.Type;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-import org.apache.logging.log4j.Logger;
-import org.javamoney.moneta.Money;
-import org.json.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
-
 import life.genny.qwanda.DateTimeDeserializer;
 import life.genny.qwanda.MoneyDeserializer;
 import life.genny.qwanda.datatype.LocalDateConverter;
 import life.genny.qwanda.entity.BaseEntity;
+import org.apache.logging.log4j.Logger;
+import org.javamoney.moneta.Money;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class JsonUtils {
 	
@@ -81,8 +69,8 @@ public class JsonUtils {
 	                      item = (T)gson.fromJson(json, clazz);
 
 	                } catch (Exception e) {
-	                	     log.debug("The JSON file received is  :::  "+json);;
-	                     log.error("Bad Deserialisation for "+clazz.getTypeName()+":"+e.getLocalizedMessage());
+//	                	log.debug("The JSON file received is  :::  "+json);;
+	                	log.error("Bad Deserialisation for "+clazz.getTypeName()+":"+e.getLocalizedMessage());
 	                }
 	        }
 	        return item;
