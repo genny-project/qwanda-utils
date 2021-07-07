7245ee3887c7d6682058d23920eb165fd830a180
Adam Crow <adamcrow63@gmail.com>
2021-07-07 18:40:05 +1000

    Updated getDEF

--- src/main/java/life/genny/utils/BaseEntityUtils.java
+++ src/main/java/life/genny/utils/BaseEntityUtils.java
@@ -2761,96 +2761,102 @@ public class BaseEntityUtils implements Serializable {
         return attribute;
     }
 
-    public BaseEntity getDEF(final BaseEntity be) {
-        Set<EntityAttribute> newMerge = new HashSet<>();
-        List<EntityAttribute> isAs = be.findPrefixEntityAttributes("PRI_IS_");
-        if (isAs.size() == 1) {
-            // Easy
-            BaseEntity defBe = RulesUtils.defs.get(be.getRealm())
-                    .get("DEF_" + isAs.get(0).getAttributeCode().substring("PRI_IS_".length()));
-            return defBe;
-        } else if (isAs.isEmpty()) {
-            // THIS HANDLES CURRENT BAD BEs
-            if (be.getCode().startsWith("CPY_")) {
-                BaseEntity defBe = RulesUtils.defs.get(be.getRealm()).get("DEF_COMPANY");
-                return defBe;
-            } else if (be.getCode().startsWith("PER_")) {
-                BaseEntity defBe = RulesUtils.defs.get(be.getRealm()).get("DEF_PERSON");
-                return defBe;
-            } else if (be.getCode().startsWith("BEG_")) {
-                BaseEntity defBe = RulesUtils.defs.get(be.getRealm()).get("DEF_INTERNSHIP");
-                return defBe;
-            } else if (be.getCode().startsWith("JNL_")) {
-                BaseEntity defBe = RulesUtils.defs.get(be.getRealm()).get("DEF_JOURNAL");
-                return defBe;
-            } else if (be.getCode().startsWith("RUL_")) {
-                BaseEntity defBe = RulesUtils.defs.get(be.getRealm()).get("DEF_RULE");
-                return defBe;
-            } else if (be.getCode().startsWith("ROL_")) {
-                BaseEntity defBe = RulesUtils.defs.get(be.getRealm()).get("DEF_ROLE");
-                return defBe;
-            } else if (be.getCode().startsWith("BKT_")) {
-                BaseEntity defBe = RulesUtils.defs.get(be.getRealm()).get("DEF_BUCKET_PAGE");
-                return defBe;
-            } else if (be.getCode().startsWith("APT_")) {
-                BaseEntity defBe = RulesUtils.defs.get(be.getRealm()).get("DEF_APPOINTMENT");
-                return defBe;
-            } else if (be.getCode().startsWith("DEV_")) {
-                BaseEntity defBe = RulesUtils.defs.get(be.getRealm()).get("DEF_DEVICE");
-                return defBe;
-            } else if (be.getCode().startsWith("FRM_")) {
-                BaseEntity defBe = RulesUtils.defs.get(be.getRealm()).get("DEF_FORM");
-                return defBe;
-            }
+	public BaseEntity getDEF(final BaseEntity be) {
+		Set<EntityAttribute> newMerge = new HashSet<>();
+		List<EntityAttribute> isAs = be.findPrefixEntityAttributes("PRI_IS_");
+		if (isAs.size() == 1) {
+			// Easy
+			BaseEntity defBe = RulesUtils.defs.get(be.getRealm())
+					.get("DEF_" + isAs.get(0).getAttributeCode().substring("PRI_IS_".length()));
+			return defBe;
+		} else if (isAs.isEmpty()) {
+			// THIS HANDLES CURRENT BAD BEs
+			if (be.getCode().startsWith("CPY_")) {
+				BaseEntity defBe = RulesUtils.defs.get(be.getRealm()).get("DEF_COMPANY");
+				return defBe;
+			} else if (be.getCode().startsWith("PER_")) {
+				BaseEntity defBe = RulesUtils.defs.get(be.getRealm()).get("DEF_PERSON");
+				return defBe;
+			} else if (be.getCode().startsWith("BEG_")) {
+				BaseEntity defBe = RulesUtils.defs.get(be.getRealm()).get("DEF_INTERNSHIP");
+				return defBe;
+			} else if (be.getCode().startsWith("JNL_")) {
+				BaseEntity defBe = RulesUtils.defs.get(be.getRealm()).get("DEF_JOURNAL");
+				return defBe;
+			} else if (be.getCode().startsWith("RUL_")) {
+				BaseEntity defBe = RulesUtils.defs.get(be.getRealm()).get("DEF_RULE");
+				return defBe;
+			} else if (be.getCode().startsWith("ROL_")) {
+				BaseEntity defBe = RulesUtils.defs.get(be.getRealm()).get("DEF_ROLE");
+				return defBe;
+			} else if (be.getCode().startsWith("BKT_")) {
+				BaseEntity defBe = RulesUtils.defs.get(be.getRealm()).get("DEF_BUCKET_PAGE");
+				return defBe;
+			} else if (be.getCode().startsWith("APT_")) {
+				BaseEntity defBe = RulesUtils.defs.get(be.getRealm()).get("DEF_APPOINTMENT");
+				return defBe;
+			} else if (be.getCode().startsWith("DEV_")) {
+				BaseEntity defBe = RulesUtils.defs.get(be.getRealm()).get("DEF_DEVICE");
+				return defBe;
+			} else if (be.getCode().startsWith("FRM_")) {
+				BaseEntity defBe = RulesUtils.defs.get(be.getRealm()).get("DEF_FORM");
+				return defBe;
+			}
 
-            else if (be.getCode().startsWith("APP_")) {
-                BaseEntity defBe = RulesUtils.defs.get(be.getRealm()).get("DEF_APPLICATION");
-                if (defBe == null) {
-                    log.error("NO DEF ASSOCIATED WITH APP be " + be.getCode());
-                    return new BaseEntity("ERR_DEF", "No DEF");
-                }
-                return defBe;
-            }
+			else if (be.getCode().startsWith("APP_")) {
+				BaseEntity defBe = RulesUtils.defs.get(be.getRealm()).get("DEF_APPLICATION");
+				if (defBe == null) {
+					log.error("NO DEF ASSOCIATED WITH APP be " + be.getCode());
+					return new BaseEntity("ERR_DEF", "No DEF");
+				}
+				return defBe;
+			}
 
-            log.error("NO DEF ASSOCIATED WITH be " + be.getCode());
-            return new BaseEntity("ERR_DEF", "No DEF");
-        } else {
-            // Create sorted merge code
-            String mergedCode = "DEF_" + isAs.stream().sorted(Comparator.comparing(EntityAttribute::getAttributeCode))
-                    .map(ea -> ea.getAttributeCode()).collect(Collectors.joining("_"));
-            BaseEntity mergedBe = RulesUtils.defs.get(be.getRealm()).get(mergedCode);
-            if (mergedBe == null) {
-                log.info("Detected NEW Combination DEF - " + mergedCode);
-                // Get primary PRI_IS
-                Optional<EntityAttribute> topDog = be.getHighestEA("PRI_IS_");
-                if (topDog.isPresent()) {
-                    String topCode = topDog.get().getAttributeCode().substring("PRI_IS_".length());
-                    BaseEntity defTopDog = RulesUtils.defs.get(be.getRealm()).get("DEF_" + topCode);
-                    mergedBe = new BaseEntity(mergedCode, mergedCode); // So this combination DEF inherits top dogs name
-                    // now copy all the combined DEF eas.
-                    for (EntityAttribute isea : isAs) {
-                        BaseEntity defEa = RulesUtils.defs.get(be.getRealm())
-                                .get("DEF_" + isea.getAttributeCode().substring("PRI_IS_".length()));
-                        for (EntityAttribute ea : defEa.getBaseEntityAttributes()) {
-                            try {
-                                mergedBe.addAttribute(ea);
-                            } catch (BadDataException e) {
-                                log.error("Bad data in getDEF ea merge " + mergedCode);
-                            }
-                        }
-                    }
-                    RulesUtils.defs.get(be.getRealm()).put(mergedCode, mergedBe);
-                    return mergedBe;
+			log.error("NO DEF ASSOCIATED WITH be " + be.getCode());
+			return new BaseEntity("ERR_DEF", "No DEF");
+		} else {
+			// Create sorted merge code
+			String mergedCode = "DEF_" + isAs.stream().sorted(Comparator.comparing(EntityAttribute::getAttributeCode))
+					.map(ea -> ea.getAttributeCode()).collect(Collectors.joining("_"));
+			mergedCode = mergedCode.replaceAll("_PRI_IS_DELETED", "");
+			BaseEntity mergedBe = RulesUtils.defs.get(be.getRealm()).get(mergedCode);
+			if (mergedBe == null) {
+				log.info("Detected NEW Combination DEF - " + mergedCode);
+				// Get primary PRI_IS
+				Optional<EntityAttribute> topDog = be.getHighestEA("PRI_IS_");
+				if (topDog.isPresent()) {
+					String topCode = topDog.get().getAttributeCode().substring("PRI_IS_".length());
+					BaseEntity defTopDog = RulesUtils.defs.get(be.getRealm()).get("DEF_" + topCode);
+					mergedBe = new BaseEntity(mergedCode,mergedCode); // So this combination DEF inherits top dogs name
+					// now copy all the combined DEF eas.
+					for (EntityAttribute isea : isAs) {
+						BaseEntity defEa = RulesUtils.defs.get(be.getRealm())
+								.get("DEF_" + isea.getAttributeCode().substring("PRI_IS_".length()));
+						if (defEa != null) {
+							for (EntityAttribute ea : defEa.getBaseEntityAttributes()) {
+								try {
+									mergedBe.addAttribute(ea);
+								} catch (BadDataException e) {
+									log.error("Bad data in getDEF ea merge " + mergedCode);
+								}
+							}
+						} else {
+							log.info(
+									"No DEF code -> " + "DEF_" + isea.getAttributeCode().substring("PRI_IS_".length()));
+							return null;
+						}
+					}
+					RulesUtils.defs.get(be.getRealm()).put(mergedCode, mergedBe);
+					return mergedBe;
 
-                } else {
-                    log.error("NO DEF EXISTS FOR " + be.getCode());
-                    return null;
-                }
-            } else {
-                return mergedBe; // return 'merged' composite
-            }
-        }
+				} else {
+					log.error("NO DEF EXISTS FOR " + be.getCode());
+					return null;
+				}
+			} else {
+				return mergedBe; // return 'merged' composite
+			}
+		}
 
-    }
-
+	}
 }
