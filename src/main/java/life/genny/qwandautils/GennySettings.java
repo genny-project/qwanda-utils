881caa359b398cddfeada2f3551143eaa3a39fae
Adam Crow <adamcrow63@gmail.com>
2021-10-16 11:40:46 +1100

    Updated GennySetting sto handle dev mode DDT

--- src/main/java/life/genny/qwandautils/GennySettings.java
+++ src/main/java/life/genny/qwandautils/GennySettings.java
@@ -17,9 +17,9 @@ public class GennySettings {
 	public static int ACCESS_TOKEN_EXPIRY_LIMIT_SECONDS = 60;
 	
 
-	public static String hostIP = System.getenv("HOSTIP") != null ? System.getenv("HOSTIP") : System.getenv("MYIP");   // remember to set up this local IP on the host
+	public static String hostIP = System.getenv("HOSTIP") != null ? System.getenv("HOSTIP") : (System.getenv("MYIP") != null ? System.getenv("MYIP"): "alyson.genny.life");   // remember to set up this local IP on the host
 	public static String myIP = System.getenv("MYIP") != null ? System.getenv("MYIP") : System.getenv("HOSTIP");   // remember to set up this local IP on the host
-	public static String cacheApiPort = System.getenv("CACHE_API_PORT") != null ? System.getenv("CACHE_API_PORT") : "8089";
+	public static String cacheApiPort = System.getenv("CACHE_API_PORT") != null ? System.getenv("CACHE_API_PORT") : "8280";
 	public static String apiPort = System.getenv("API_PORT") != null ? System.getenv("API_PORT") : "8088";
 	public static String pontoonPort = System.getenv("PONTOON_PORT") != null ? System.getenv("PONTOON_PORT") : "8086";
 	public static String webhookPort = System.getenv("WEBHOOK_PORT") != null ? System.getenv("WEBHOOK_PORT") : "9123";
