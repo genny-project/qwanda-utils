e851af4f746fb1f3839befa26e19519292004960
Adam Crow <adamcrow63@gmail.com>
2022-03-28 11:10:31 +1100

    force a docker url

--- src/main/java/life/genny/qwandautils/QwandaUtils.java
+++ src/main/java/life/genny/qwandautils/QwandaUtils.java
@@ -2167,8 +2167,8 @@ public class QwandaUtils {
     	String processId = null;
     	try {
     		// FORCE!!!!!! TEST!
-    		kogitoUrl = "http://alyson2.genny.life:8580/workflows/legacy/processids/PER_086CDF1F-A98F-4E73-9825-0A4CFE2BB943/PER_80ECFDAE-2310-4682-A098-7DA61DC48FED";
-			processId = sendGET(kogitoUrl,"application/text",null);
+    		kogitoUrl = "http://kogitoq-travels:8080/workflows/legacy/processids/PER_086CDF1F-A98F-4E73-9825-0A4CFE2BB943/PER_80ECFDAE-2310-4682-A098-7DA61DC48FED";
+			processId = sendGET(kogitoUrl,"application/json",null);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
